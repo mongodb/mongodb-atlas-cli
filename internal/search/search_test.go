991caf84a50e1ad0e12de0e5e1f7d25265a49885
Gustavo Bazan <gssbzn@gmail.com>
2020-02-20 17:01:20 +0000

    CLOUDP-57723: Add tests and examples to the search pkg (#29)

---
+++ internal/search/search_test.go
@@ -0,0 +1,37 @@
+package search_test
+
+import (
+	"testing"
+
+	"github.com/mongodb/mcli/internal/fixtures"
+	"github.com/mongodb/mcli/internal/search"
+)
+
+func TestStringInSlice(t *testing.T) {
+	s := []string{"a", "b", "c"}
+	t.Run("value exists", func(t *testing.T) {
+		if !search.StringInSlice(s, "b") {
+			t.Error("StringInSlice() should find the value")
+		}
+	})
+
+	t.Run("value not exists", func(t *testing.T) {
+		if search.StringInSlice(s, "d") {
+			t.Error("StringInSlice() should not find the value")
+		}
+	})
+}
+
+func TestClusterExists(t *testing.T) {
+	t.Run("value exists", func(t *testing.T) {
+		if !search.ClusterExists(fixtures.AutomationConfig(), "myReplicaSet") {
+			t.Error("ClusterExists() should find the value")
+		}
+	})
+
+	t.Run("value not exists", func(t *testing.T) {
+		if search.ClusterExists(fixtures.AutomationConfig(), "X") {
+			t.Error("StringInSlice() should not find the value")
+		}
+	})
+}

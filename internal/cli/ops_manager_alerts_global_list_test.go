40016ce7a457d232c3e8b4c41eac5ca858dc881a
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2020-03-25 16:11:16 +0000

    CLOUDP-57846: List global alerts in mongocli (#67)

---
+++ internal/cli/ops_manager_alerts_global_list_test.go
@@ -0,0 +1,52 @@
+// Copyright 2020 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	om "github.com/mongodb/go-client-mongodb-ops-manager/opsmngr"
+	"github.com/mongodb/mongocli/internal/fixtures"
+	"github.com/mongodb/mongocli/internal/mocks"
+)
+
+func TestOpsManagerAlertsGlobalList_Run(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockStore := mocks.NewMockGlobalAlertLister(ctrl)
+
+	defer ctrl.Finish()
+
+	expected := fixtures.GlobalAlerts()
+
+	alertOpts := om.AlertsListOptions{
+		Status: "OPEN",
+	}
+
+	mockStore.
+		EXPECT().GlobalAlerts(&alertOpts).
+		Return(expected, nil).
+		Times(1)
+
+	opts := &opsManagerAlertsGlobalListOpts{
+		store:  mockStore,
+		status: "OPEN",
+	}
+
+	err := opts.Run()
+	if err != nil {
+		t.Fatalf("Run() unexpected error: %v", err)
+	}
+}

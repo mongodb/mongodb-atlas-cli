10d3736d512acab32752eb7589e957e757307880
Gustavo Bazan <gssbzn@gmail.com>
2020-04-16 11:36:03 +0100

    CLOUDP-60870: Lists process disks/paritions, OM

---
+++ internal/store/host_disks.go
@@ -0,0 +1,39 @@
+// Copyright 2020 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package store
+
+import (
+	"context"
+	"fmt"
+
+	atlas "github.com/mongodb/go-client-mongodb-atlas/mongodbatlas"
+	om "github.com/mongodb/go-client-mongodb-ops-manager/opsmngr"
+	"github.com/mongodb/mongocli/internal/config"
+)
+
+type HostDisksLister interface {
+	HostDisks(string, string, *atlas.ListOptions) (*atlas.ProcessDisksResponse, error)
+}
+
+// HostDisks encapsulate the logic to manage different cloud providers
+func (s *Store) HostDisks(groupID, hostID string, opts *atlas.ListOptions) (*atlas.ProcessDisksResponse, error) {
+	switch s.service {
+	case config.OpsManagerService, config.CloudManagerService:
+		result, _, err := s.client.(*om.Client).HostDisks.List(context.Background(), groupID, hostID, opts)
+		return result, err
+	default:
+		return nil, fmt.Errorf("unsupported service: %s", s.service)
+	}
+}

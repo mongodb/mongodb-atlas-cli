108ccf66d28c9dcc2770c7203727b02a6e5cc738
Gustavo Bazan <gssbzn@gmail.com>
2020-05-05 14:09:22 +0100

    CLOUDP-62033: Generate JSON for download center (#151)

---
+++ internal/release/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
+type DownloadArchive struct {
+	PreviousReleasesLink string     `json:"previous_releases_link"`
+	ReleaseDate          time.Time  `json:"release_date"`
+	ReleaseNotesLink     string     `json:"release_notes_link"`
+	TutorialLink         string     `json:"tutorial_link"`
+	Version              string     `json:"version"`
+	ManualLink           string     `json:"manual_link"`
+	Platform             []Platform `json:"platform"`
+}
+
+type Platform struct {
+	Arch          string  `json:"arch"`
+	OS            string  `json:"os"`
+	PackageFormat string  `json:"package_format"`
+	Packages      Package `json:"packages"`
+}
+
+type Package struct {
+	Title string `json:"title"`
+	Links []Link `json:"links"`
+}
+
+type Link struct {
+	DownloadLink string `json:"download_link"`
+	Name         string `json:"name"`
+}
+
+func newPlatform(version, arch, os, distro, format string) *Platform {
+	p := &Platform{}
+	p.Arch = arch
+	p.OS = distro
+	p.PackageFormat = format
+	p.Packages = Package{
+		Title: "MongoDB CLI",
+		Links: []Link{
+			{
+				DownloadLink: fmt.Sprintf("https://downloads.mongodb.com/on-prem-mms/mcli/mongocli_%s_%s_%s.%s", version, os, arch, format),
+				Name:         format,
+			},
+		},
+	}
+	return p
+}
+
+func main() {
+	version := os.Args[1]
+	feedFilename := fmt.Sprintf("mongocli_%s.json", version)
+	fmt.Printf("Generating JSON: %s\n", feedFilename)
+	feedFile, err := os.Create(feedFilename)
+	if err != nil {
+		fmt.Println("error creating file")
+		os.Exit(1)
+	}
+
+	downloadArchive := &DownloadArchive{
+		ReleaseDate:          time.Now().UTC(),
+		ManualLink:           fmt.Sprintf("https://docs.mongodb.com/mongocli/v%s/", version),
+		PreviousReleasesLink: "https://github.com/mongodb/mongocli/releases",
+		ReleaseNotesLink:     fmt.Sprintf("https://docs.mongodb.com/mongocli/v%s/release-notes/", version),
+		TutorialLink:         fmt.Sprintf("https://docs.mongodb.com/mongocli/v%s/quick-start/", version),
+		Platform: []Platform{
+			*newPlatform(version, "x86_64", "linux", "Debian 9 / Ubuntu 16.04 + 18.04", "deb"),
+			*newPlatform(version, "x86_64", "linux", "Red Hat + CentOS 6, 7, 8 / SUSE 12 + 15 / Amazon Linux", "rpm"),
+			*newPlatform(version, "x86_64", "windows", "Microsoft Windows", "zip"),
+			*newPlatform(version, "x86_64", "macOS", "macOS", "tar.gz"),
+			*newPlatform(version, "x86_64", "linux", "Linux (x86_64)", "tar.gz"),
+		},
+	}
+
+	defer feedFile.Close()
+
+	jsonEncoder := json.NewEncoder(feedFile)
+	jsonEncoder.SetIndent("", "  ")
+	err = jsonEncoder.Encode(downloadArchive)
+
+	if err != nil {
+		fmt.Println("error encoding file")
+		os.Exit(1)
+	}
+	fmt.Printf("File %s has been generated\n", feedFilename)
+}

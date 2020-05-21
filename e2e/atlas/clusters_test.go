42f8f4b35cd3987e732630cc9a989c6759693d36
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-05-21 15:18:38 +0100

    Added e2e test for atlas watch

--- e2e/atlas/clusters_test.go
+++ e2e/atlas/clusters_test.go
@@ -22,6 +22,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
@@ -71,6 +72,24 @@ func TestAtlasClusters(t *testing.T) {
 		}
 
 		ensureCluster(t, cluster, clusterName, "4.0", 10)
+	})
+
+	t.Run("Watch cluster deployment", func(t *testing.T) {
+		cmd := exec.Command(cliPath,
+			atlasEntity,
+			clustersEntity,
+			"watch",
+			clusterName)
+		cmd.Env = os.Environ()
+		resp, err := cmd.CombinedOutput()
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v, resp: %v", err, string(resp))
+		}
+
+		if !strings.Contains(string(resp), "Cluster available at:") {
+			t.Errorf("got=%#v\nwant=%#v\n", string(resp), "Cluster available at:d")
+		}
 	})
 
 	t.Run("List", func(t *testing.T) {

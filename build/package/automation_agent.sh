6e025cc1baef26817404a60a309fb4008e11b0e2
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-05-27 16:45:45 +0100

    Updated automation_agent.sh

--- build/package/automation_agent.sh
+++ build/package/automation_agent.sh
@@ -17,14 +17,14 @@ replace_property_in_file() {
 
     # Set the new property
     temp_file=$(mktemp)
-    grep -vE "^\\s*${2}\\s*=" "${1}" > "${temp_file}" # Export contents minus any lines containing the specified property
-    echo "${2}=${3}" >> "${temp_file}"                # Set the new property value
-    cat "${temp_file}" > "${1}"                       # Replace the contents of the original file, while preserving any permissions
-    rm "${temp_file}"
+    sudo grep -vE "^\\s*${2}\\s*=" "${1}" > "${temp_file}" # Export contents minus any lines containing the specified property
+    sudo echo "${2}=${3}" >> "${temp_file}"                # Set the new property value
+    sudo cat "${temp_file}" > "${1}"                       # Replace the contents of the original file, while preserving any permissions
+    sudo rm "${temp_file}"
 }
 
 echo "Installing dependeces"
-sudo -y apt-get install libcurl3 libgssapi-krb5-2 \
+sudo apt-get -y install libcurl3 libgssapi-krb5-2 \
      libkrb5-dbg libldap-2.4-2 libpci3 libsasl2-2 snmp \
      liblzma5 openssl
 
@@ -37,7 +37,6 @@ sudo dpkg -i mongodb-mms-automation-agent-manager_10.15.0.6409-1_amd64.ubuntu160
 echo "Replacing mmsGroupId and mmsApiKey properties"
 replace_property_in_file "/etc/mongodb-mms/automation-agent.config" "mmsGroupId" "$mmsGroupId"
 replace_property_in_file "/etc/mongodb-mms/automation-agent.config" "mmsApiKey" "$mmsApiKey"
-replace_property_in_file "/etc/mongodb-mms/automation-agent.config" "mmsBaseUrl" "https://api-agents-dev.mongodb.com"
 
 echo "Preparing the /data directory to store your MongoDB data"
 sudo mkdir -p /data

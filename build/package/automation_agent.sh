cb5a909a4acea89196fb40405375542c57cac185
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-05-27 15:49:46 +0100

    Updated script

--- build/package/automation_agent.sh
+++ build/package/automation_agent.sh
@@ -23,17 +23,23 @@ replace_property_in_file() {
     rm "${temp_file}"
 }
 
+echo "Installing dependeces"
+sudo -y apt-get install libcurl3 libgssapi-krb5-2 \
+     libkrb5-dbg libldap-2.4-2 libpci3 libsasl2-2 snmp \
+     liblzma5 openssl
+
 clear
 
-echo "Download and extract the automation agent"
+echo "Downloadind and extracting the automation agent"
 curl -OL https://cloud-dev.mongodb.com/download/agent/automation/mongodb-mms-automation-agent-manager_10.15.0.6409-1_amd64.ubuntu1604.deb
 sudo dpkg -i mongodb-mms-automation-agent-manager_10.15.0.6409-1_amd64.ubuntu1604.deb
 
 echo "Replacing mmsGroupId and mmsApiKey properties"
 replace_property_in_file "/etc/mongodb-mms/automation-agent.config" "mmsGroupId" "$mmsGroupId"
 replace_property_in_file "/etc/mongodb-mms/automation-agent.config" "mmsApiKey" "$mmsApiKey"
+replace_property_in_file "/etc/mongodb-mms/automation-agent.config" "mmsBaseUrl" "https://api-agents-dev.mongodb.com"
 
-echo "Preparing the /data directory to store your MongoDB data. This directory must be owned by the mongod user"
+echo "Preparing the /data directory to store your MongoDB data"
 sudo mkdir -p /data
 sudo chown mongod:mongod /data
 

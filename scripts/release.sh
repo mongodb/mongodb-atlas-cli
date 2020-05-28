d230c82962cf505fc88b68bca25dcb58440dbbf0
Gustavo Bazan <gssbzn@gmail.com>
2020-05-28 16:47:34 +0100

    task: add a releasing script

---
+++ scripts/release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VERSION="${1-}"
+
+if [[ -z "${VERSION}" ]]; then
+    echo "Please provide a version"
+    exit 1
+fi
+
+read -p "Are you sure to release v${VERSION}? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+git tag -a -s "v${VERSION}" -m "v${VERSION}"
+git push origin "v${VERSION}"
+evergreen patch -p mongocli-master -y -d "Release ${VERSION}" -v release_publish -v release_msi -t all -f

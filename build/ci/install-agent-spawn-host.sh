41fb9d6f324540e456bde7d951928c0ae2a651c9
Gustavo Bazan <gssbzn@gmail.com>
2020-06-10 11:09:06 +0100

    task: improve on flakyness of spawn host

--- build/ci/install-agent-spawn-host.sh
+++ build/ci/install-agent-spawn-host.sh
@@ -18,36 +18,18 @@
 set -euo pipefail
 
 while getopts 'i:h:g:u:a:b:' opt; do
-  case $opt in
-  i) keyfile="$OPTARG" ;; # SSH identity file
-  u) user="$OPTARG" ;; # Username on the remote host
-  h) hostsfile="$OPTARG" ;; # Output of Evergreen host.list
-  g) groupid="$OPTARG" ;; # APIGroupId
-  a) apiKey="$OPTARG" ;; # APIKey
-  b) baseUrl="$OPTARG";; #Cloud Manager URL
+  case ${opt} in
+  i) keyfile="${OPTARG}" ;; # SSH identity file
+  u) user="${OPTARG}" ;; # Username on the remote host
+  h) hostsFile="${OPTARG}" ;; # Output of Evergreen host.list
   *) exit 1 ;;
   esac
 done
 
-declare -a ssh_opts
-ssh_opts[0]="-o"
-ssh_opts[1]="UserKnownHostsFile=/dev/null"
-ssh_opts[2]="-o"
-ssh_opts[3]="StrictHostKeyChecking=no"
-ssh_opts[4]="-q"
-ssh_opts[5]="-o"
-ssh_opts[6]="ConnectTimeout=30"
-ssh_opts[7]="-o"
-ssh_opts[8]="TCPKeepAlive=yes"
-ssh_opts[9]="-o"
-ssh_opts[10]="ServerAliveInterval=15"
-ssh_opts[11]="-o"
-ssh_opts[12]="ServerAliveCountMax=20"
-ssh_opts[13]="-i"
-ssh_opts[14]="$keyfile"
+export SSH_OPTS="-i ${keyfile} -o SendEnv=LC_GROUP_ID -o SendEnv=LC_AGENT_KEY"
 
 hosts=$(
-  cat <<EOF | python - "$hostsfile"
+  cat <<EOF | python - "${hostsFile}"
 import sys
 import json
 with open(sys.argv[1]) as hostsfile:
@@ -56,31 +38,12 @@ with open(sys.argv[1]) as hostsfile:
         print(host["dns_name"])
 EOF
 )
-for host in $hosts; do
-  set +e
-  echo "Installing dependeces on $host"
-  ssh "${ssh_opts[@]}" -tt "$user@$host" 'bash -s' <<'ENDSSH'
-        sudo apt-get update && \
-        sudo apt-get install -y --no-install-recommends ca-certificates curl logrotate openssl snmp && exit
-ENDSSH
-  echo "Installing the automation agent on $host"
-  ssh "${ssh_opts[@]}" -tt "$user@$host" ARG1="$groupid" ARG2="$apiKey" ARG3="$baseUrl" 'bash -s' <<'ENDSSH'
-        echo "Downloadind and extracting the automation agent"
-        curl -OL ${ARG3}download/agent/automation/mongodb-mms-automation-agent-manager_latest_amd64.ubuntu1604.deb
-        sudo dpkg -i mongodb-mms-automation-agent-manager_latest_amd64.ubuntu1604.deb
+for host in ${hosts}; do
+    echo "Seeding ${host}"
+    ./ego seed "${user}@${host}"
+    # start the app server on the remote host. Choose port 9443 as it is open on the security group.
+    echo "bin/ego scenario_install_agent"
+    ./ego run "${user}@${host}" bin/ego scenario_install_agent \
+                                                  --baseUrl "${BASE_URL}"
 
-        echo "Replacing mmsGroupId and mmsApiKey"
-        sudo sed -i "s/\(mmsGroupId *= *\).*/\1$ARG1/" /etc/mongodb-mms/automation-agent.config
-        sudo sed -i "s/\(mmsApiKey *= *\).*/\1$ARG2/" /etc/mongodb-mms/automation-agent.config
-
-        echo "Preparing the /data directory to store your MongoDB data"
-        sudo mkdir -p /data
-        sudo chown mongod:mongod /data
-
-        echo "Starting the agent"
-        sudo systemctl start mongodb-mms-automation-agent.service
-        exit
-ENDSSH
-  echo "Storing ${host} in e2e/cloud_manager/e2e.env"
-  sudo sed "s/\(HOSTNAME *= *\).*/\1${host}/" e2e/cloud_manager/e2e.env.example > e2e/cloud_manager/e2e.env
 done
--- build/ci/ssh-ready.sh
+++ build/ci/ssh-ready.sh
@@ -21,12 +21,12 @@ declare -i connection_attempts
 declare -ri timeout=10
 
 while getopts 'i:h:t:u:' opt; do
-    case $opt in
-        i) keyfile="$OPTARG";;              # SSH identity file
-        u) user="$OPTARG";;                 # Username on the remote host
-        h) hostsfile="$OPTARG";;            # Output of Evergreen host.list
-        t) connection_attempts="$OPTARG";;  # How many times to attempt to connect via SSH
-        *) exit 1;;
+    case ${opt} in
+        i) keyfile="${OPTARG}";;              # SSH identity file
+        u) user="${OPTARG}";;                 # Username on the remote host
+        h) hostsfile="${OPTARG}";;            # Output of Evergreen host.list
+        t) connection_attempts="${OPTARG}";;  # How many times to attempt to connect via SSH
+        *) echo "Invalid option"; exit 1;;
     esac
 done
 
@@ -46,8 +46,8 @@ connection_attempts=${connection_attempts:-60} # Total timeout = timeout * timeo
 for host in $hosts; do
     set +e
     echo "Waiting for $host to become available..."
-    while ! ssh -i "$keyfile" -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt "$user@$host" exit 2> /dev/null; do
-        if [ "$attempts" -ge "$connection_attempts" ]; then
+    while ! ssh -i "$keyfile" -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt "${user}@${host}" exit 2> /dev/null; do
+        if [[ "${attempts}" -ge "${connection_attempts}" ]]; then
             echo 'Connect to spawn host failed'
             exit 1
         fi

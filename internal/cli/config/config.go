74a82fee6acb4320351d8521cba95a62a4751e49
Gustavo Bazan <gssbzn@gmail.com>
2020-06-25 16:37:25 +0100

    Update internal/cli/config/config.go
    Co-authored-by: Melissa Mahoney <melissa.mahoney@mongodb.com>

--- internal/cli/config/config.go
+++ internal/cli/config/config.go
@@ -97,7 +97,7 @@ Enter [?] on any option to get help.
 	}
 
 	fmt.Printf("\nYour profile is now configured.\n", config.ToolName)
-	fmt.Printf("You can use [%s config set] to change more settings. \n", config.ToolName)
+	fmt.Printf("You can use [%s config set] to change these settings at a later time.\n", config.ToolName)
 	if config.Name() != config.DefaultProfile {
 		fmt.Printf("To use this profile remember to set the flag [-%s %s]\n", flag.ProfileShort, config.Name())
 	}

4a1800513b9380e3c7c50eb7d156326686c97ad4
Rob Carlan <rob.carlan@10gen.com>
2020-07-08 16:25:53 -0400

    create datalake

--- internal/store/data_lake.go
+++ internal/store/data_lake.go
@@ -33,7 +33,7 @@ type DataLakeDescriber interface {
 }
 
 type DataLakeCreator interface {
-	CreateDataLake(request *atlas.DataLakeCreateRequest) (*atlas.DataLake, error)
+	CreateDataLake(string, *atlas.DataLakeCreateRequest) (*atlas.DataLake, error)
 }
 
 type DataLakeDeleter interface {

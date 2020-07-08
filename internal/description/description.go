9509630b081252df550f4a889ba9c0217eb3d919
Rob Carlan <rob.carlan@10gen.com>
2020-07-08 17:20:01 -0400

    add data lake delete

--- internal/description/description.go
+++ internal/description/description.go
@@ -43,6 +43,7 @@ const (
 	ListDataLakes            = "List data lakes for your project."
 	DescribeDataLakes        = "Return a specific data lake."
 	CreateDataLakes          = "Create a new data lake."
+	DeleteDataLake           = "Delete a data lake."
 	Clusters                 = "Manage clusters for your project."
 	ClustersIndexes          = "Manage cluster rolling indexes for your project."
 	ClustersLong             = "The clusters command provides access to your cluster configurations. You can create, edit, and delete clusters."

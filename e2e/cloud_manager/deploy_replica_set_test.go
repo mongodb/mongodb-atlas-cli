e4d8c7eab828c65162c08d62bae27042d81c238b
Gustavo Bazan <gssbzn@gmail.com>
2020-07-16 12:18:53 +0100

    Fixes

--- e2e/cloud_manager/deploy_replica_set_test.go
+++ e2e/cloud_manager/deploy_replica_set_test.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// +build e2e cloudmanager,clusters
+// +build e2e cloudmanager,deploy_replica_set
 
 package cloud_manager_test
 

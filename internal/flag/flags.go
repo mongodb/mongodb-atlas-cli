bc3829c2857b8a6bd5a06208a7bc50297f8fb672
Gustavo Bazan <gssbzn@gmail.com>
2020-07-22 20:15:56 +0100

    Fix duplicated flag

--- internal/flag/flags.go
+++ internal/flag/flags.go
@@ -42,8 +42,8 @@ const (
 	Email                           = "email"                           // Email flag
 	Out                             = "out"                             // Out flag
 	Output                          = "output"                          // Output flag
+	OutputShort                     = "o"                               // OutputShort flag
 	Minutes                         = "minutes"                         // Minutes flag
-	OutShort                        = "o"                               // OutShort flag
 	Status                          = "status"                          // Status flag
 	Start                           = "start"                           // Start flag
 	End                             = "end"                             // End flag

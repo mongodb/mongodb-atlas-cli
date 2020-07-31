83571850a5ddabf8daf5bec46c460f2c65f27bf8
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2020-07-31 11:22:22 +0200

    CLOUDP-67926: mongocli iam organizations|orgs apiKey(s)|apikey(s) describe <id> (#316)

--- internal/store/api_keys.go
+++ internal/store/api_keys.go
@@ -23,7 +23,7 @@ import (
 	"go.mongodb.org/ops-manager/opsmngr"
 )
 
-//go:generate mockgen -destination=../mocks/api_keys.go -package=mocks github.com/mongodb/mongocli/internal/store ProjectAPIKeyLister,OrganizationAPIKeyLister
+//go:generate mockgen -destination=../mocks/api_keys.go -package=mocks github.com/mongodb/mongocli/internal/store ProjectAPIKeyLister,OrganizationAPIKeyLister,OrganizationAPIKeyDescriber
 
 type ProjectAPIKeyLister interface {
 	ProjectAPIKeys(string, *atlas.ListOptions) ([]atlas.APIKey, error)
@@ -33,7 +33,11 @@ type OrganizationAPIKeyLister interface {
 	OrganizationAPIKeys(string, *atlas.ListOptions) ([]atlas.APIKey, error)
 }
 
-// OrganizationAPIKeys encapsulate the logic to manage different cloud providers
+type OrganizationAPIKeyDescriber interface {
+	OrganizationAPIKey(string, string) (*atlas.APIKey, error)
+}
+
+// OrganizationAPIKeys encapsulates the logic to manage different cloud providers
 func (s *Store) OrganizationAPIKeys(orgID string, opts *atlas.ListOptions) ([]atlas.APIKey, error) {
 	switch s.service {
 	case config.CloudService:
@@ -55,6 +59,20 @@ func (s *Store) ProjectAPIKeys(projectID string, opts *atlas.ListOptions) ([]atl
 		return result, err
 	case config.OpsManagerService, config.CloudManagerService:
 		result, _, err := s.client.(*opsmngr.Client).ProjectAPIKeys.List(context.Background(), projectID, opts)
+		return result, err
+	default:
+		return nil, fmt.Errorf("unsupported service: %s", s.service)
+	}
+}
+
+// OrganizationAPIKey encapsulates the logic to manage different cloud providers
+func (s *Store) OrganizationAPIKey(orgID, apiKeyID string) (*atlas.APIKey, error) {
+	switch s.service {
+	case config.CloudService:
+		result, _, err := s.client.(*atlas.Client).APIKeys.Get(context.Background(), orgID, apiKeyID)
+		return result, err
+	case config.OpsManagerService, config.CloudManagerService:
+		result, _, err := s.client.(*opsmngr.Client).OrganizationAPIKeys.Get(context.Background(), orgID, apiKeyID)
 		return result, err
 	default:
 		return nil, fmt.Errorf("unsupported service: %s", s.service)

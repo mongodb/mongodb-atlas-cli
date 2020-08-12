8d00a90aa80ca53eb4fba351e8717bc931e6a0f2
Gustavo Bazan <gssbzn@gmail.com>
2020-08-12 12:00:26 +0100

    Update internal/cli/atlas/privateendpoints/interfaces/create.go
    Co-authored-by: Andrea Angiolillo <andrea.angiolillo@mongodb.com>

--- internal/cli/atlas/privateendpoints/interfaces/create.go
+++ internal/cli/atlas/privateendpoints/interfaces/create.go
@@ -68,7 +68,7 @@ func CreateBuilder() *cobra.Command {
 
 	cmd.Flags().StringVar(&opts.ProjectID, flag.ProjectID, "", usage.ProjectID)
 
-	_ = cmd.MarkFlagRequired(flag.Region)
+_ = cmd.MarkFlagRequired(flag. PrivateEndpointID)
 
 	return cmd
 }

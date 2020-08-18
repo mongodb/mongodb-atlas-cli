6e963ac0d499adb34da82f6610526faf4f995e54
Gustavo Bazan <gssbzn@gmail.com>
2020-08-18 09:27:47 +0100

    CLOUDP-69557: reduce datalake e2e flakyness

--- e2e/atlas/data_lake_test.go
+++ e2e/atlas/data_lake_test.go
@@ -32,7 +32,7 @@ func TestDataLakes(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	datalakeName := fmt.Sprintf("e2e-data-lake-%v", n)
+	dataLakeName := fmt.Sprintf("e2e-data-lake-%v", n)
 
 	cliPath, err := e2e.Bin()
 	if err != nil {
@@ -44,7 +44,7 @@ func TestDataLakes(t *testing.T) {
 			atlasEntity,
 			datalakeEntity,
 			"create",
-			datalakeName,
+			dataLakeName,
 			"-o=json")
 		cmd.Env = os.Environ()
 		resp, err := cmd.CombinedOutput()
@@ -52,9 +52,9 @@ func TestDataLakes(t *testing.T) {
 		a := assert.New(t)
 		a.NoError(err, string(resp))
 
-		var datalake atlas.DataLake
-		if err = json.Unmarshal(resp, &datalake); a.NoError(err) {
-			a.Equal(datalakeName, datalake.Name)
+		var dataLake atlas.DataLake
+		if err = json.Unmarshal(resp, &dataLake); a.NoError(err) {
+			a.Equal(dataLakeName, dataLake.Name)
 		}
 	})
 
@@ -63,7 +63,7 @@ func TestDataLakes(t *testing.T) {
 			atlasEntity,
 			datalakeEntity,
 			"describe",
-			datalakeName,
+			dataLakeName,
 			"-o=json")
 		cmd.Env = os.Environ()
 		resp, err := cmd.CombinedOutput()
@@ -71,9 +71,31 @@ func TestDataLakes(t *testing.T) {
 		a := assert.New(t)
 		a.NoError(err, string(resp))
 
-		var datalake atlas.DataLake
-		if err = json.Unmarshal(resp, &datalake); a.NoError(err) {
-			a.Equal(datalakeName, datalake.Name)
+		var dataLake atlas.DataLake
+		if err = json.Unmarshal(resp, &dataLake); a.NoError(err) {
+			a.Equal(dataLakeName, dataLake.Name)
+		}
+	})
+
+	t.Run("Update", func(t *testing.T) {
+		const updateRegion = "VIRGINIA_USA"
+		cmd := exec.Command(cliPath,
+			atlasEntity,
+			datalakeEntity,
+			"update",
+			dataLakeName,
+			"--region",
+			updateRegion,
+			"-o=json")
+		cmd.Env = os.Environ()
+		resp, err := cmd.CombinedOutput()
+
+		a := assert.New(t)
+		a.NoError(err, string(resp))
+
+		var dataLake atlas.DataLake
+		if err = json.Unmarshal(resp, &dataLake); a.NoError(err) {
+			a.Equal(updateRegion, dataLake.DataProcessRegion.Region)
 		}
 	})
 
@@ -94,41 +116,19 @@ func TestDataLakes(t *testing.T) {
 		}
 	})
 
-	t.Run("Update", func(t *testing.T) {
-		const updateRegion = "VIRGINIA_USA"
-		cmd := exec.Command(cliPath,
-			atlasEntity,
-			datalakeEntity,
-			"update",
-			datalakeName,
-			"--region",
-			updateRegion,
-			"-o=json")
-		cmd.Env = os.Environ()
-		resp, err := cmd.CombinedOutput()
-
-		a := assert.New(t)
-		a.NoError(err, string(resp))
-
-		var datalake atlas.DataLake
-		if err = json.Unmarshal(resp, &datalake); a.NoError(err) {
-			a.Equal(updateRegion, datalake.DataProcessRegion.Region)
-		}
-	})
-
 	t.Run("Delete", func(t *testing.T) {
 		cmd := exec.Command(cliPath,
 			atlasEntity,
 			datalakeEntity,
 			"delete",
-			datalakeName,
+			dataLakeName,
 			"--force")
 		cmd.Env = os.Environ()
 
 		resp, err := cmd.CombinedOutput()
 		a := assert.New(t)
 		a.NoError(err, string(resp))
-		expected := fmt.Sprintf("Data Lake '%s' deleted\n", datalakeName)
+		expected := fmt.Sprintf("Data Lake '%s' deleted\n", dataLakeName)
 		a.Equal(expected, string(resp))
 	})
 }

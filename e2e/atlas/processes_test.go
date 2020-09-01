43f11ebbbc260fbdb3740d7882d473088528f255
Gustavo Bazan <gssbzn@gmail.com>
2020-09-01 11:15:03 +0100

    CLOUDP-70718: Test atlas process list

---
+++ e2e/atlas/processes_test.go
@@ -0,0 +1,66 @@
+// Copyright 2020 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// +build e2e atlas,processes
+
+package atlas_test
+
+import (
+	"encoding/json"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/mongodb/mongocli/e2e"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/atlas/mongodbatlas"
+)
+
+func TestProcesses(t *testing.T) {
+	clusterName, err := deployCluster()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	defer func() {
+		if e := deleteCluster(clusterName); e != nil {
+			t.Errorf("error deleting test cluster: %v", e)
+		}
+	}()
+
+	cliPath, err := e2e.Bin()
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	t.Run("list", func(t *testing.T) {
+		cmd := exec.Command(cliPath,
+			atlasEntity,
+			processesEntity,
+			"list",
+			"-o=json")
+
+		cmd.Env = os.Environ()
+		resp, err := cmd.CombinedOutput()
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v, resp: %v", err, string(resp))
+		}
+
+		var indexes []*mongodbatlas.Process
+		if err := json.Unmarshal(resp, &indexes); assert.NoError(t, err) {
+			assert.NotEmpty(t, indexes)
+		}
+	})
+}

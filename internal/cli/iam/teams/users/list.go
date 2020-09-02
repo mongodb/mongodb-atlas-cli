7a32d67a1995ac0e24e499f90e553d16c667cf61
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-09-02 12:25:51 +0200

    Update list.go

--- internal/cli/iam/teams/users/list.go
+++ internal/cli/iam/teams/users/list.go
@@ -23,8 +23,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const listTemplate = `ID	NAME{{range .}}
-{{.ID}}	{{.Name}}{{end}}
+const listTemplate = `ID	FIRST NAME	LAST NAME	USERNAME	EMAIL{{range .}}
+{{.ID}}	{{.FirstName}}	{{.LastName}}	{{.Username}}	{{.EmailAddress}}{{end}}
 `
 
 type ListOpts struct {

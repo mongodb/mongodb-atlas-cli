2e92d23e02f0ddf96bb0b20c28b200d3fc9827cd
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-09-02 08:37:52 +0200

    Update list_test.go

--- internal/cli/iam/organizations/users/list_test.go
+++ internal/cli/iam/organizations/users/list_test.go
@@ -18,9 +18,8 @@ package users
 import (
 	"testing"
 
-	"github.com/mongodb/mongocli/internal/cli"
-
 	"github.com/golang/mock/gomock"
+	"github.com/mongodb/mongocli/internal/cli"
 	"github.com/mongodb/mongocli/internal/mocks"
 	"go.mongodb.org/atlas/mongodbatlas"
 )

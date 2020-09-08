5804ce1ffa97269c04c8a23adabcab008dae4892
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-09-08 09:39:29 +0200

    Update delete_test.go

--- internal/cli/atlas/integrations/delete_test.go
+++ internal/cli/atlas/integrations/delete_test.go
@@ -18,10 +18,9 @@ package integrations
 import (
 	"testing"
 
-	"github.com/mongodb/mongocli/internal/flag"
-
 	"github.com/golang/mock/gomock"
 	"github.com/mongodb/mongocli/internal/cli"
+	"github.com/mongodb/mongocli/internal/flag"
 	"github.com/mongodb/mongocli/internal/mocks"
 )
 

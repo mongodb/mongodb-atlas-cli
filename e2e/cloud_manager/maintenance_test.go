6d7f1b27f7d93cdb698de718ad4361f2dd21f37b
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-09-09 15:42:29 +0200

    Update maintenance_test.go

--- e2e/cloud_manager/maintenance_test.go
+++ e2e/cloud_manager/maintenance_test.go
@@ -169,7 +169,7 @@ func TestMaintenanceWindows(t *testing.T) {
 		a := assert.New(t)
 		a.NoError(err, string(resp))
 
-		expected := fmt.Sprintf("Maintenance window '%s' deleted.\n", maintenanceWindowID)
+		expected := fmt.Sprintf("Maintenance window '%s' deleted\n", maintenanceWindowID)
 		a.Equal(expected, string(resp))
 	})
 }

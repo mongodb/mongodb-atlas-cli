5f8277d6746ac6c733166c02f2bfca04595a2c2c
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-09-15 12:58:33 +0200

    Update list_test.go

--- internal/cli/performanceadvisor/namespaces/list_test.go
+++ internal/cli/performanceadvisor/namespaces/list_test.go
@@ -25,7 +25,7 @@ import (
 	"go.mongodb.org/atlas/mongodbatlas"
 )
 
-func TestDBUserList_Run(t *testing.T) {
+func TestNamespacesList_Run(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	mockStore := mocks.NewMockPerformanceAdvisorNamespacesLister(ctrl)
 	defer ctrl.Finish()

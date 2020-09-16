40b80316a004fcd773e83700d6596a33f83bcef8
Gustavo Bazan <gssbzn@gmail.com>
2020-09-16 18:05:37 +0100

    CLOUDP-73125: mongocli atlas networking peering watch doe snot work for AWS

--- internal/cli/atlas/networking/peering/watch.go
+++ internal/cli/atlas/networking/peering/watch.go
@@ -36,12 +36,22 @@ func (opts *WatchOpts) initStore() error {
 	return err
 }
 
+const (
+	waitingForUser    = "WAITING_FOR_USER"
+	failed            = "FAILED"
+	available         = "AVAILABLE"
+	pendingAcceptance = "PENDING_ACCEPTANCE"
+)
+
 func (opts *WatchOpts) watcher() (bool, error) {
 	result, err := opts.store.PeeringConnection(opts.ConfigProjectID(), opts.id)
 	if err != nil {
 		return false, err
 	}
-	return result.Status == "WAITING_FOR_USER" || result.Status == "FAILED" || result.Status == "AVAILABLE", nil
+	if result.Status != "" {
+		return result.Status == waitingForUser || result.Status == failed || result.Status == available, nil
+	}
+	return result.StatusName == pendingAcceptance || result.StatusName == failed || result.StatusName == available, nil
 }
 
 func (opts *WatchOpts) Run() error {

8d66e29506e65c4a92b3da40374886db2cca19c8
Gustavo Bazan <gssbzn@gmail.com>
2020-09-17 10:28:38 +0100

    CLOUDP-73125: mongocli atlas networking peering watch doe snot work for AWS (#449)

--- internal/cli/atlas/networking/peering/watch_test.go
+++ internal/cli/atlas/networking/peering/watch_test.go
@@ -20,30 +20,54 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/mongodb/mongocli/internal/cli"
+	"github.com/mongodb/mongocli/internal/flag"
 	"github.com/mongodb/mongocli/internal/mocks"
 	"go.mongodb.org/atlas/mongodbatlas"
 )
 
-func TestWatch_Run(t *testing.T) {
+func TestWatchBuilder(t *testing.T) {
+	cli.CmdValidator(t, WatchBuilder(), 0, []string{flag.ProjectID, flag.Output})
+}
+
+func TestWatchOpts_Run(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	mockStore := mocks.NewMockPeeringConnectionDescriber(ctrl)
 	defer ctrl.Finish()
 
-	describeOpts := &WatchOpts{
-		id:    "test",
-		store: mockStore,
+	tests := []struct {
+		name     string
+		expected *mongodbatlas.Peer
+	}{
+		{
+			name:     "AWS",
+			expected: &mongodbatlas.Peer{StatusName: "PENDING_ACCEPTANCE"},
+		},
+		{
+			name:     "AZURE",
+			expected: &mongodbatlas.Peer{Status: "AVAILABLE"},
+		},
+		{
+			name:     "GCP",
+			expected: &mongodbatlas.Peer{Status: "WAITING_FOR_USER"},
+		},
 	}
+	for _, tt := range tests {
+		expected := tt.expected
+		t.Run(tt.name, func(t *testing.T) {
+			describeOpts := &WatchOpts{
+				id:    "test",
+				store: mockStore,
+			}
+			mockStore.
+				EXPECT().
+				PeeringConnection(describeOpts.ProjectID, describeOpts.id).
+				Return(expected, nil).
+				Times(1)
 
-	expected := &mongodbatlas.Peer{Status: "WAITING_FOR_USER"}
-
-	mockStore.
-		EXPECT().
-		PeeringConnection(describeOpts.ProjectID, describeOpts.id).
-		Return(expected, nil).
-		Times(1)
-
-	err := describeOpts.Run()
-	if err != nil {
-		t.Fatalf("Run() unexpected error: %v", err)
+			if err := describeOpts.Run(); err != nil {
+				t.Fatalf("Run() unexpected error: %v", err)
+			}
+		})
 	}
 }

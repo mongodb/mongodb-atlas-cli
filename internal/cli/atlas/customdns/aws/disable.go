db601e824a4f38d21724debfde4605b36620ed96
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2020-09-23 14:33:37 +0200

    CLOUDP-69429: Cloud Services Milestone: Custom DNS AWS (#453)

---
+++ internal/cli/atlas/customdns/aws/disable.go
@@ -0,0 +1,69 @@
+// Copyright 2020 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aws
+
+import (
+	"github.com/mongodb/mongocli/internal/cli"
+	"github.com/mongodb/mongocli/internal/config"
+	"github.com/mongodb/mongocli/internal/flag"
+	"github.com/mongodb/mongocli/internal/store"
+	"github.com/mongodb/mongocli/internal/usage"
+	"github.com/spf13/cobra"
+)
+
+type DisableOpts struct {
+	cli.GlobalOpts
+	cli.OutputOpts
+	store store.CustomDNSDisabler
+}
+
+var disableTemplate = "DNS configuration disabled.\n"
+
+func (opts *DisableOpts) initStore() error {
+	var err error
+	opts.store, err = store.New(config.Default())
+	return err
+}
+
+func (opts *DisableOpts) Run() error {
+	r, err := opts.store.DisableCustomDNS(opts.ConfigProjectID())
+	if err != nil {
+		return err
+	}
+	return opts.Print(r)
+}
+
+// mongocli atlas customDns aws disable [--projectId projectId]
+func DisableBuilder() *cobra.Command {
+	opts := &DisableOpts{}
+	cmd := &cobra.Command{
+		Use:   "disable",
+		Short: disable,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return opts.PreRunE(
+				opts.initStore,
+				opts.InitOutput(cmd.OutOrStdout(), disableTemplate),
+			)
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return opts.Run()
+		},
+	}
+
+	cmd.Flags().StringVar(&opts.ProjectID, flag.ProjectID, "", usage.ProjectID)
+	cmd.Flags().StringVarP(&opts.Output, flag.Output, flag.OutputShort, "", usage.FormatOut)
+
+	return cmd
+}

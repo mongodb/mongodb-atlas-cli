139ad139f1bcd8c00595294da8ac3d364353bfc1
Gustavo Bazan <gssbzn@gmail.com>
2020-10-08 17:54:57 +0100

    Update internal/store/monitoring.go
    Co-authored-by: Andrea Angiolillo <andrea.angiolillo@mongodb.com>

--- internal/store/monitoring.go
+++ internal/store/monitoring.go
@@ -43,7 +43,7 @@ func (s *Store) StartMonitoring(groupID string, host *opsmngr.Host) (*opsmngr.Ho
 	}
 }
 
-// StopMonitoring encapsulate the logic to manage different cloud providers
+// StopMonitoring encapsulates the logic to manage different cloud providers
 func (s *Store) StopMonitoring(groupID, hostID string) error {
 	switch s.service {
 	case config.OpsManagerService, config.CloudManagerService:

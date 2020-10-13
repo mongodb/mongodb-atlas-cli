9c46a2791bc4e49ed79865e2b9c4c3f14a28d621
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-10-13 09:57:27 +0100

    Refactoring

--- internal/cli/opsmanager/admin/backup/filesystem/create.go
+++ internal/cli/opsmanager/admin/backup/filesystem/create.go
@@ -88,7 +88,7 @@ func CreateBuilder() *cobra.Command {
 
 	cmd.Flags().StringVarP(&opts.Output, flag.Output, flag.OutputShort, "", usage.FormatOut)
 
-	_ = cmd.MarkFlagRequired(flag.ID)
+	_ = cmd.MarkFlagRequired(flag.Name)
 	_ = cmd.MarkFlagRequired(flag.StorePath)
 	_ = cmd.MarkFlagRequired(flag.MMAPV1CompressionSetting)
 	_ = cmd.MarkFlagRequired(flag.WTCompressionSetting)

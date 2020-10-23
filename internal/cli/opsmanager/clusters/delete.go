365b4f79467bdd499779245f8271798770544f56
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-10-23 17:36:27 +0100

    Update delete.go

--- internal/cli/opsmanager/clusters/delete.go
+++ internal/cli/opsmanager/clusters/delete.go
@@ -89,8 +89,6 @@ func DeleteBuilder() *cobra.Command {
 	}
 
 	cmd.Flags().BoolVar(&opts.Confirm, flag.Force, false, usage.Force)
-
-	cmd.Flags()
 	cmd.Flags().StringVar(&opts.ProjectID, flag.ProjectID, "", usage.ProjectID)
 
 	return cmd

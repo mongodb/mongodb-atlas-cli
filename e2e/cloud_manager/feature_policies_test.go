e7bd8f2db8ad9a3284a0949765fa0b98e2d3f435
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-11-27 16:29:01 +0000

    Addressed PR comments

--- e2e/cloud_manager/feature_policies_test.go
+++ e2e/cloud_manager/feature_policies_test.go
@@ -17,6 +17,7 @@ package cloud_manager_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"os/exec"
 	"testing"
@@ -29,39 +30,27 @@ func TestFeaturePolicies(t *testing.T) {
 	const policyExternallyManagedLock = "EXTERNALLY_MANAGED_LOCK"
 	const policyDisableUserManagement = "DISABLE_USER_MANAGEMENT"
 
+	n, err := e2e.RandInt(255)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	projectName := fmt.Sprintf("e2e-maintenance-proj-%v", n)
+	projectID, err := createProject(projectName)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
 	cliPath, err := e2e.Bin()
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
-	var externalManagSystemName string
-
-	t.Run("List", func(t *testing.T) {
-		cmd := exec.Command(cliPath,
-			entity,
-			featurePolicies,
-			"list",
-			"-o=json",
-		)
-
-		cmd.Env = os.Environ()
-		resp, err := cmd.CombinedOutput()
-
-		if err != nil {
-			t.Fatalf("unexpected error: %v, resp: %v", err, string(resp))
+	defer func() {
+		if e := deleteProject(projectID); e != nil {
+			t.Errorf("error deleting project: %v", e)
 		}
-
-		var policy *opsmngr.FeaturePolicy
-		if err := json.Unmarshal(resp, &policy); err != nil {
-			t.Fatalf("unexpected error: %v", err)
-		}
-
-		if len(policy.Policies) == 0 {
-			t.Error("No policies found")
-		}
-
-		externalManagSystemName = policy.ExternalManagementSystem.Name
-	})
+	}()
 
 	t.Run("Update", func(t *testing.T) {
 		cmd := exec.Command(cliPath,
@@ -69,12 +58,14 @@ func TestFeaturePolicies(t *testing.T) {
 			featurePolicies,
 			"update",
 			"--name",
-			externalManagSystemName,
+			"test",
 			"--policy",
 			policyExternallyManagedLock,
 			"--policy",
 			policyDisableUserManagement,
 			"-o=json",
+			"--projectId",
+			projectID,
 		)
 
 		cmd.Env = os.Environ()
@@ -89,8 +80,8 @@ func TestFeaturePolicies(t *testing.T) {
 			t.Fatalf("unexpected error: %v", err)
 		}
 
-		if len(policy.Policies) == 0 {
-			t.Error("No policies found")
+		if len(policy.Policies) != 2 {
+			t.Error("Policy count mismatch")
 		}
 
 		foundExternalManagedLock := false
@@ -109,6 +100,33 @@ func TestFeaturePolicies(t *testing.T) {
 
 		if !foundDisableUserManagement {
 			t.Errorf("policy %s not found", policyDisableUserManagement)
+		}
+	})
+
+	t.Run("List", func(t *testing.T) {
+		cmd := exec.Command(cliPath,
+			entity,
+			featurePolicies,
+			"list",
+			"-o=json",
+			"--projectId",
+			projectID,
+		)
+
+		cmd.Env = os.Environ()
+		resp, err := cmd.CombinedOutput()
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v, resp: %v", err, string(resp))
+		}
+
+		var policy *opsmngr.FeaturePolicy
+		if err := json.Unmarshal(resp, &policy); err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if len(policy.Policies) == 0 {
+			t.Error("No policies found")
 		}
 	})
 }

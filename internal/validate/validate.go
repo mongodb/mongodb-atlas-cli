861fdf31349b16f7db29b1c440ade8f63e2ddc4c
Gustavo Bazan <gus.bazan@hey.com>
2021-08-19 13:43:42 +0100

    CLOUDP-95953: Selecting a mongosh path is not optional on the config command

--- internal/validate/validate.go
+++ internal/validate/validate.go
@@ -131,6 +131,17 @@ func Path(val interface{}) error {
 	return nil
 }
 
+func OptionalPath(val interface{}) error {
+	if val == nil {
+		return nil
+	}
+	s, err := toString(val)
+	if err != nil || s == "" {
+		return err
+	}
+	return Path(val)
+}
+
 var ErrInvalidClusterName = errors.New("invalid cluster name")
 
 func ClusterName(val interface{}) error {

0b31bdbe02de6e09dd8cf505ebe6cae3d7dd67d6
andreaangiolillo <andrea.angiolillo@mongodb.com>
2021-09-10 17:21:19 +0100

    updated build tags

--- e2e/iam/project_teams_test.go
+++ e2e/iam/project_teams_test.go
@@ -11,7 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// +build e2e iam
+//go:build e2e || iam || (iam && opsmanager)
+// +build e2e iam iam,opsmanager
 
 package iam_test
 
--- e2e/iam/teams_test.go
+++ e2e/iam/teams_test.go
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// +build e2e iam
-// +build e2e opsmanager
+//go:build e2e || iam || (iam && opsmanager)
+// +build e2e iam iam,opsmanager
 
 package iam_test
 

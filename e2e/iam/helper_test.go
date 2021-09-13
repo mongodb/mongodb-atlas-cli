0506977f818947e64082382c5d03ae0a9c9fabc5
andreaangiolillo <andrea.angiolillo@mongodb.com>
2021-09-13 09:54:17 +0100

    updated tags in helper

--- e2e/iam/helper_test.go
+++ e2e/iam/helper_test.go
@@ -11,7 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// +build e2e iam
+//go:build e2e || iam || iam_mms_50
+// +build e2e iam iam_mms_50
 
 package iam_test
 

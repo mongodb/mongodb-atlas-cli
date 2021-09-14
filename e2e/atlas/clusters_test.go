ebb13aa8891131aece0ea95ff95897a2bfd7ab49
andreaangiolillo <andrea.angiolillo@mongodb.com>
2021-09-14 13:26:07 +0100

    updated build tags e2e tests

--- e2e/atlas/clusters_test.go
+++ e2e/atlas/clusters_test.go
@@ -11,9 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 //go:build e2e || (atlas && clusters && !m0)
-// +build e2e atlas,clusters,!m0
 
 package atlas_test
 

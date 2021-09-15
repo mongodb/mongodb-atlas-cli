7d3b45909dce8b6cc2a9eb65edd0e51def94916c
andreaangiolillo <andrea.angiolillo@mongodb.com>
2021-09-15 11:21:56 +0100

    removed opsmanager tag

--- e2e/cloud_manager/maintenance_test.go
+++ e2e/cloud_manager/maintenance_test.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//go:build e2e || (cloudmanager && generic) || (opsmanager && generic)
+//go:build e2e || (cloudmanager && generic) || (om44 && generic) || (om50 && generic)
 
 package cloud_manager_test
 

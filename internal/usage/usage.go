e50bc30ac2a03ea6a5af82cf8d4bca3bb9f26f69
andreaangiolillo <andrea.angiolillo@mongodb.com>
2021-09-15 15:07:35 +0100

    CLOUDP-98165: Allow mongocli to create gov only projects

--- internal/usage/usage.go
+++ internal/usage/usage.go
@@ -264,6 +264,7 @@ const (
 	ContainerRegion                 = "Atlas region where the container resides."
 	ContainerRegions                = "List of Atlas regions where the container resides."
 	ProjectOwnerID                  = "Unique 24-hexadecimal digit string that identifies the Atlas user account to be granted the Project Owner role on the specified project."
+	GovCloudRegionsOnly             = "Make the project gov region only."
 	ReclaimFreeSpaceTimestamp       = "Timestamp in ISO 8601 format when the service reclaims the space. If not set, defaults to the current timestamp."
 	QuickstartDefault               = "Run the Quickstart command with all the auto generated values to deploy and access an Atlas cluster."
 	ServerlessProvider              = "Cloud service provider that applies to the provisioned the serverless instance."

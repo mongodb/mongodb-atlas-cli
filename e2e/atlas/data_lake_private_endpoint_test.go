85dae0e0bf5d8456d9bca331cbb0a0ff374ce02a
andreaangiolillo <andrea.angiolillo@mongodb.com>
2022-01-13 17:31:08 +0000

    Added e2e tetst

---
+++ e2e/atlas/data_lake_private_endpoint_test.go
@@ -0,0 +1,107 @@
+// Copyright 2022 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//go:build e2e || (atlas && networking)
+
+package atlas_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/mongodb/mongocli/e2e"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	atlas "go.mongodb.org/atlas/mongodbatlas"
+)
+
+func TestDataLakePrivateEndpointsAWS(t *testing.T) {
+	g := newAtlasE2ETestGenerator(t)
+	g.generateProject("dataLakePrivateEndpointsAWS")
+
+	n, err := e2e.RandInt(int64(1000))
+	require.NoError(t, err)
+
+	cliPath, err := e2e.Bin()
+	require.NoError(t, err)
+
+	vpcID := fmt.Sprintf("vpce-0fcd9d80bbafe%n", n)
+
+	var id string
+
+	t.Run("Create", func(t *testing.T) {
+		cmd := exec.Command(cliPath,
+			atlasEntity,
+			privateEndpointsEntity,
+			datalakeEntity,
+			awsEntity,
+			"create",
+			"--privateEndpointId="+vpcID,
+			"--projectId",
+			g.projectID,
+			"-o=json")
+		cmd.Env = os.Environ()
+
+		a := assert.New(t)
+		if resp, err := cmd.CombinedOutput(); a.NoError(err, string(resp)) {
+			var r atlas.PrivateEndpointConnection
+			if err = json.Unmarshal(resp, &r); a.NoError(err) {
+				id = vpcID
+			}
+		}
+	})
+
+	t.Run("List", func(t *testing.T) {
+		cmd := exec.Command(cliPath,
+			atlasEntity,
+			privateEndpointsEntity,
+			datalakeEntity,
+			awsEntity,
+			"ls",
+			"--projectId",
+			g.projectID,
+			"-o=json")
+		cmd.Env = os.Environ()
+		resp, err := cmd.CombinedOutput()
+
+		a := assert.New(t)
+		a.NoError(err, string(resp))
+		var r []atlas.PrivateEndpointConnection
+		if err = json.Unmarshal(resp, &r); a.NoError(err) {
+			a.NotEmpty(r)
+		}
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		cmd := exec.Command(cliPath,
+			atlasEntity,
+			privateEndpointsEntity,
+			datalakeEntity,
+			awsEntity,
+			"delete",
+			vpcID,
+			"--projectId",
+			g.projectID,
+			"--force")
+		cmd.Env = os.Environ()
+
+		resp, err := cmd.CombinedOutput()
+		a := assert.New(t)
+		a.NoError(err, string(resp))
+		expected := fmt.Sprintf("Private endpoint '%s' deleted\n", id)
+		a.Equal(expected, string(resp))
+	})
+}

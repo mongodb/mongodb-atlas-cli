1c73e880d87184da11c188185a1ddc335fe21d65
andreaangiolillo <andrea.angiolillo@mongodb.com>
2022-01-13 09:07:16 +0000

    addressed doc review

--- internal/cli/atlas/privateendpoints/datalake/data_lake.go
+++ internal/cli/atlas/privateendpoints/datalake/data_lake.go
@@ -25,7 +25,7 @@ func Builder() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     use,
 		Aliases: cli.GenerateAliases(use),
-		Short:   "Manage Data Lake Private Endpoints.",
+		Short:   "Manage Data Lake private endpoints.",
 	}
 
 	cmd.AddCommand(aws.Builder())

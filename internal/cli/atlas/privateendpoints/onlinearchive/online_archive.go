7036d75e2ef36e012022ce778facc4caed2a9a8d
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2022-01-14 17:57:33 +0000

    CLOUDP-108859: Create the OnlineArchive commands under Atlas privateEndpoints (#924)

--- internal/cli/atlas/privateendpoints/onlinearchive/online_archive.go
+++ internal/cli/atlas/privateendpoints/onlinearchive/online_archive.go
@@ -1,4 +1,4 @@
-// Copyright 2021 MongoDB Inc
+// Copyright 2022 MongoDB Inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,22 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build unit
-// +build unit
-
-package privateendpoints
+package onlinearchive
 
 import (
-	"testing"
-
-	"github.com/mongodb/mongocli/internal/test"
+	"github.com/mongodb/mongocli/internal/cli"
+	"github.com/mongodb/mongocli/internal/cli/atlas/privateendpoints/datalake/aws"
+	"github.com/spf13/cobra"
 )
 
-func TestBuilder(t *testing.T) {
-	test.CmdValidator(
-		t,
-		Builder(),
-		12,
-		[]string{},
-	)
+func Builder() *cobra.Command {
+	const use = "onlineArchive"
+	cmd := &cobra.Command{
+		Use:     use,
+		Aliases: cli.GenerateAliases(use),
+		Short:   "Manage Online Archive private endpoints.",
+	}
+
+	cmd.AddCommand(aws.Builder())
+
+	return cmd
 }
--- internal/cli/atlas/privateendpoints/onlinearchive/online_archive_test.go
+++ internal/cli/atlas/privateendpoints/onlinearchive/online_archive_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 MongoDB Inc
+// Copyright 2022 MongoDB Inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 //go:build unit
 // +build unit
 
-package privateendpoints
+package onlinearchive
 
 import (
 	"testing"
@@ -27,7 +27,7 @@ func TestBuilder(t *testing.T) {
 	test.CmdValidator(
 		t,
 		Builder(),
-		12,
+		1,
 		[]string{},
 	)
 }

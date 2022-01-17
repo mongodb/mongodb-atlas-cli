42060ec4ab115608cd3970663a35378bb2df3dcd
Bianca <bianca.vianadeaguiar@mongodb.com>
2022-01-17 15:23:33 +0000

    CLOUDP-108867: Create one private endpoint for Google Cloud in an Atlas project

--- internal/cli/atlas/privateendpoints/gcp/gcp.go
+++ internal/cli/atlas/privateendpoints/gcp/gcp.go
@@ -25,7 +25,10 @@ func Builder() *cobra.Command {
 		Use:   "gcp",
 		Short: "Manage GCP private endpoints.",
 	}
-	cmd.AddCommand(ListBuilder())
+	cmd.AddCommand(
+		CreateBuilder(),
+		ListBuilder(),
+	)
 
 	return cmd
 }

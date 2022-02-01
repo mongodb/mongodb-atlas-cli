9f8416c98bcb351facc717237a52baa21ab0726f
Gustavo Bazan <gus.bazan@hey.com>
2022-02-01 12:37:50 +0000

    fix: fix spacing on copy notice

--- e2e/cloud_manager/events_test.go
+++ e2e/cloud_manager/events_test.go
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 //go:build e2e || (cloudmanager && generic)
 
 package cloud_manager_test

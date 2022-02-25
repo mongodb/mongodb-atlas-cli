94ae6f22a2ab8bf9f5692fbb7a32c26fd06480a5
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2022-02-25 10:56:31 +0000

    CLOUDP-113788: [Mongocli] Improve the release process by building only the correct binary (#998)

--- build/package/rename-pkg.sh
+++ build/package/rename-pkg.sh
@@ -28,13 +28,19 @@ mkdir yum apt
 
 # we could generate a similar name with goreleaser but we want to keep the vars evg compatible to use later
 if [[ "${package_name:?}" == mongocli ]]; then
+  ECHO "Renaming ${FILENAME}.deb to apt/mongodb-cli${unstable-}_${VERSION}${latest_deb-}_amd64.deb"
   cp "$FILENAME.deb" apt/
   mv "apt/$FILENAME.deb" "apt/mongodb-cli${unstable-}_${VERSION}${latest_deb-}_amd64.deb"
+
+  ECHO "Renaming ${FILENAME}.rpm to yum/mongodb-cli${unstable-}-${VERSION}${latest_rpm-}.x86_64.rpm"
   cp "$FILENAME.rpm" yum/
   mv "yum/$FILENAME.rpm" "yum/mongodb-cli${unstable-}-${VERSION}${latest_rpm-}.x86_64.rpm"
 else
+  ECHO "Renaming ${FILENAME}.deb to apt/mongodb-atlas-cli${unstable-}_${VERSION}${latest_deb-}_amd64.deb"
   cp "$FILENAME.deb" apt/
   mv "apt/$FILENAME.deb" "apt/mongodb-atlas-cli${unstable-}_${VERSION}${latest_deb-}_amd64.deb"
+
+  ECHO "Renaming ${FILENAME}.rpm to yum/mongodb-atlas-cli${unstable-}-${VERSION}${latest_rpm-}.x86_64.rpm"
   cp "$FILENAME.rpm" yum/
   mv "yum/$FILENAME.rpm" "yum/mongodb-atlas-cli${unstable-}-${VERSION}${latest_rpm-}.x86_64.rpm"
 fi

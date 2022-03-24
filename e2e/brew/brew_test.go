3109c24a30996a69255d1b49f3eacbad25f1dda4
andreaangiolillo <andrea.angiolillo@mongodb.com>
2022-03-24 11:55:38 +0000

    Update brew_test.go

--- e2e/brew/brew_test.go
+++ e2e/brew/brew_test.go
@@ -63,7 +63,7 @@ func TestMongoCLIConfig(t *testing.T) {
 		got := strings.TrimSpace(string(resp))
 		want := errorMessage
 
-		if !strings.HasPrefix(got, want) {
+		if !strings.Contains(got, want) {
 			t.Errorf("want '%s'; got '%s'\n", want, got)
 		}
 	})
@@ -102,7 +102,7 @@ func TestAtlasCLIConfig(t *testing.T) {
 		got := strings.TrimSpace(string(resp))
 		want := errorMessage
 
-		if !strings.HasPrefix(got, want) {
+		if !strings.Contains(got, want) {
 			t.Errorf("want '%s'; got '%s'\n", want, got)
 		}
 	})

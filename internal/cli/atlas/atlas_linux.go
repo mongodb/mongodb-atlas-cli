78abaf71256b7bf31d855dd964f14bbbc48d0af2
andreaangiolillo <andrea.angiolillo@mongodb.com>
2022-03-28 17:30:38 +0100

    Update atlas_linux.go

--- internal/cli/atlas/atlas_linux.go
+++ internal/cli/atlas/atlas_linux.go
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//go:build !windows || !darwin
-// +build !windows !darwin
+//go:build !windows && !darwin
+// +build !windows,!darwin
 
 package atlas
 

5eca6bf73d1dc07a498775f27036a0abd4a7f4ac
Filipe Menezes <filipe.menezes@mongodb.com>
2022-03-29 13:02:49 +0100

    update comment

--- internal/cli/atlas/logs/logs.go
+++ internal/cli/atlas/logs/logs.go
@@ -20,7 +20,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// MongoCLIBuilder is used in mongocli until atlascli is released.
+// MongoCLIBuilder is used in "mongocli atlas" to avoid adding new command to deprecated cli.
 func MongoCLIBuilder() *cobra.Command {
 	const use = "logs"
 	cmd := &cobra.Command{

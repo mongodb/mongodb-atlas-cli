1ee73b3815a518adf98d5f271e2202297679220a
Bianca <bianca.vianadeaguiar@mongodb.com>
2022-05-05 21:50:33 +0100

    CLOUDP-119540: Decouple token refreshing and validation to reuse in auth commands

--- internal/cli/root/atlas/builder.go
+++ internal/cli/root/atlas/builder.go
@@ -76,6 +76,7 @@ type Notifier struct {
 
 // Builder conditionally adds children commands as needed.
 func Builder(profile *string) *cobra.Command {
+	opts := &cli.RefresherOpts{}
 	rootCmd := &cobra.Command{
 		Version: version.Version,
 		Use:     atlas,

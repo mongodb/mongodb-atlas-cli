1b944b10d80203f12a935128a6f9878c551432ee
Bianca <bianca.vianadeaguiar@mongodb.com>
2022-05-30 13:15:12 +0100

    Fix fmt

--- internal/cli/default_setter_opts.go
+++ internal/cli/default_setter_opts.go
@@ -211,7 +211,7 @@ func (opts *DefaultSetterOpts) OrgExists(id string) bool {
 
 func (opts *DefaultSetterOpts) displayInitialSelectText() {
 	if !opts.AskedOrgsOrProjects && opts.AskOrgsAndProjectsText != "" {
-		_, _ = fmt.Fprintf(opts.OutWriter, "%s",opts.AskOrgsAndProjectsText)
+		_, _ = fmt.Fprintf(opts.OutWriter, "%s", opts.AskOrgsAndProjectsText)
 	}
 }
 

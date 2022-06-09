980d7c372bbf2d9311c51ec187fc718ee828cd2d
Gustavo Bazan <gus.bazan@hey.com>
2022-06-09 17:05:03 -0400

    Update create.go

--- internal/cli/atlas/clusters/create.go
+++ internal/cli/atlas/clusters/create.go
@@ -190,7 +190,7 @@ func CreateBuilder() *cobra.Command {
 		Long: `To get started quickly, specify a name for your cluster, a cloud provider, and a region to deploy a three-member replica set with the latest MongoDB server version.
 For full control of your deployment, or to create multi-cloud clusters, provide a JSON configuration file with the --file flag.`,
 		Example: fmt.Sprintf(`  Deploy a free cluster:
-  $ %[1]s cluster create <clusterName>  --projectId <projectId> --provider AWS --region US_EAST_1 --tier M0
+  $ %[1]s cluster create <clusterName> --projectId <projectId> --provider AWS --region US_EAST_1 --tier M0
 
   Deploy a three-member replica set in AWS:
   $ %[1]s cluster create <clusterName> --projectId <projectId> --provider AWS --region US_EAST_1 --members 3 --tier M10 --mdbVersion 5.0 --diskSizeGB 10

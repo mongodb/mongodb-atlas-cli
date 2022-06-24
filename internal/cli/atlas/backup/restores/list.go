e3704d8ddae1d5ad1f65e9afb1663dffd5b9a04d
Filipe Menezes <filipe.menezes@mongodb.com>
2022-06-24 13:18:09 +0100

    fix

--- internal/cli/atlas/backup/restores/list.go
+++ internal/cli/atlas/backup/restores/list.go
@@ -44,7 +44,7 @@ func (opts *ListOpts) initStore(ctx context.Context) func() error {
 }
 
 var restoreListTemplate = `ID	SNAPSHOT	CLUSTER	TYPE	EXPIRES AT	URLs{{range .Results}}
-{{.ID}}	{{.SnapshotID}}	{{.TargetClusterName}}	{{.DeliveryType}}	{{.ExpiresAt}}	{{range $index, $element := .DeliveryURL[0]}}{{if $index}}; {{end}}{{$element}}{{end}}{{end}}
+{{.ID}}	{{.SnapshotID}}	{{.TargetClusterName}}	{{.DeliveryType}}	{{.ExpiresAt}}	{{range $index, $element := .DeliveryURL}}{{if $index}}; {{end}}{{$element}}{{end}}{{end}}
 `
 
 func (opts *ListOpts) Run() error {

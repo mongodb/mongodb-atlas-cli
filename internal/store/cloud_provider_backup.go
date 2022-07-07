515735118c993e3525981607e2080e66a77d0500
Michal Derej <michal.derej@mongodb.com>
2022-07-07 10:43:00 +0100

    CLOUDP-126257: Create command 'atlas backups exports buckets describe'

--- internal/store/cloud_provider_backup.go
+++ internal/store/cloud_provider_backup.go
@@ -21,7 +21,7 @@ import (
 	atlas "go.mongodb.org/atlas/mongodbatlas"
 )
 
-//go:generate mockgen -destination=../mocks/mock_cloud_provider_backup.go -package=mocks github.com/mongodb/mongodb-atlas-cli/internal/store RestoreJobsLister,RestoreJobsDescriber,RestoreJobsCreator,SnapshotsLister,SnapshotsCreator,SnapshotsDescriber,SnapshotsDeleter,ExportJobsLister,ExportBucketsLister,ExportBucketsCreator
+//go:generate mockgen -destination=../mocks/mock_cloud_provider_backup.go -package=mocks github.com/mongodb/mongodb-atlas-cli/internal/store RestoreJobsLister,RestoreJobsDescriber,RestoreJobsCreator,SnapshotsLister,SnapshotsCreator,SnapshotsDescriber,SnapshotsDeleter,ExportJobsLister,ExportBucketsLister,ExportBucketsCreator,ExportBucketsDescriber
 
 type RestoreJobsLister interface {
 	RestoreJobs(string, string, *atlas.ListOptions) (*atlas.CloudProviderSnapshotRestoreJobs, error)
@@ -61,6 +61,10 @@ type ExportBucketsLister interface {
 
 type ExportBucketsCreator interface {
 	CreateExportBucket(string, *atlas.CloudProviderSnapshotExportBucket) (*atlas.CloudProviderSnapshotExportBucket, error)
+}
+
+type ExportBucketsDescriber interface {
+	DescribeExportBucket(string, string) (*atlas.CloudProviderSnapshotExportBucket, error)
 }
 
 // RestoreJobs encapsulates the logic to manage different cloud providers.
@@ -198,6 +202,17 @@ func (s *Store) CreateExportBucket(projectID string, bucket *atlas.CloudProvider
 	switch s.service {
 	case config.CloudService, config.CloudGovService:
 		result, _, err := s.client.(*atlas.Client).CloudProviderSnapshotExportBuckets.Create(s.ctx, projectID, bucket)
+		return result, err
+	default:
+		return nil, fmt.Errorf("%w: %s", errUnsupportedService, s.service)
+	}
+}
+
+// DescribeExportBucket encapsulates the logic to manage different cloud providers.
+func (s *Store) DescribeExportBucket(projectID, bucketID string) (*atlas.CloudProviderSnapshotExportBucket, error) {
+	switch s.service {
+	case config.CloudService, config.CloudGovService:
+		result, _, err := s.client.(*atlas.Client).CloudProviderSnapshotExportBuckets.Get(s.ctx, projectID, bucketID)
 		return result, err
 	default:
 		return nil, fmt.Errorf("%w: %s", errUnsupportedService, s.service)

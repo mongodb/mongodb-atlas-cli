398e57e0e7a0e4dd5f63f1785ed4718e5d296010
Filipe Constantinov Menezes <filipe.menezes@mongodb.com>
2022-08-18 15:25:44 +0100

    CLOUDP-133279: Fix repo configuration for Ubuntu 22 & RHEL 9 and also fix for 6.0 repo (#1419)
    * CLOUDP-133279: Fix repo configuration for Ubuntu 22 & RHEL 9 and also fix for 6.0 repo
    
    * Add 6.0 for tests

--- tools/genevergreen/main.go
+++ tools/genevergreen/main.go
@@ -32,7 +32,7 @@ const (
 )
 
 var (
-	serverVersions = []string{"4.2", "4.4", "5.0"}
+	serverVersions = []string{"4.2", "4.4", "5.0", "6.0"}
 	oses           = []string{"amazonlinux2", "centos7", "centos8", "rhel9", "debian9", "debian10", "ubuntu18.04", "ubuntu20.04", "ubuntu22.04"}
 	repos          = []string{"org", "enterprise"}
 	postPkgImg     = map[string]string{

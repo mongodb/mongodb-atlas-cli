09e2278041a3d8b06e843471143ce96666de8141
Lander McFall Kerbey <lander.kerbey@mongodb.com>
2022-11-08 14:42:23 -0500

    (DOCSP-26347)

--- internal/cli/atlas/networking/peering/create/aws.go
+++ internal/cli/atlas/networking/peering/create/aws.go
@@ -126,7 +126,7 @@ func AwsBuilder() *cobra.Command {
 		Short: "Create a connection with AWS.",
 		Args:  require.NoArgs,
 		Example: fmt.Sprintf(`  # The following command creates a peering connection between the Atlas VPC and your AWS VPC for a project using the default profile.
-  %s atlas networking peering create aws --accountId <aws-account-id> --atlasCidrBlock 192.168.0.0/24 --region us-east-1 --routeTableCidrBlock 10.0.0.0/24 --vpcId vpc-078ac381aa90e1e63`, cli.ExampleAtlasEntryPoint()),
+  %s networking peering create aws --accountId <aws-account-id> --atlasCidrBlock 192.168.0.0/24 --region us-east-1 --routeTableCidrBlock 10.0.0.0/24 --vpcId vpc-078ac381aa90e1e63`, cli.ExampleAtlasEntryPoint()),
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			return opts.PreRunE(
 				opts.ValidateProjectID,

3e263dea9713908abf7c81dcf5389c9171e7858f
Evelyn Rabil <evelyn.rabil@mongodb.com>
2022-11-10 11:46:35 -0500

    Removes rendunant boolean.

--- internal/cli/atlas/security/ldap/save.go
+++ internal/cli/atlas/security/ldap/save.go
@@ -128,7 +128,7 @@ func SaveBuilder() *cobra.Command {
 		Use:   "save",
 		Short: "Save an LDAP configuration.",
 		Example: fmt.Sprintf(` The following example configures an LDAP server to authenticate and authorize MongoDB users.
-  %s security ldap save --authenticationEnabled true --authorizationEnabled true 
+  %s security ldap save --authenticationEnabled --authorizationEnabled 
   --hostname atlas-ldaps-01.ldap.myteam.com --bindUsername 
   "CN=Administrator,CN=Users,DC=atlas-ldaps-01,DC=myteam,DC=com" 
   --bindPassword changeMe`, cli.ExampleAtlasEntryPoint()),

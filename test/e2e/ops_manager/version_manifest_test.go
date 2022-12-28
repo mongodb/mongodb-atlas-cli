bdaa2fd8b0beceb421a911b0e849f9ef4e42c316
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2022-12-28 12:01:43 +0000

    CLOUDP-136638: Add OM 6.0 E2E tests for MongoCLI (#1579)

--- test/e2e/ops_manager/version_manifest_test.go
+++ test/e2e/ops_manager/version_manifest_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build e2e || (generic && (om44 || om50))
+//go:build e2e || (generic && (om50 || om60))
 
 package ops_manager_test
 

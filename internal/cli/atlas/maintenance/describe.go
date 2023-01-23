78ff20194681a92cdeddba34d69f4a807417a1ce
sarahsimpers <82042374+sarahsimpers@users.noreply.github.com>
2023-01-23 06:42:10 -0500

    (DOCSP-16675) Backfills docs content for maintenance windows and logs (#1614)

--- internal/cli/atlas/maintenance/describe.go
+++ internal/cli/atlas/maintenance/describe.go
@@ -16,6 +16,7 @@ package maintenance
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/mongodb/mongodb-atlas-cli/internal/cli"
 	"github.com/mongodb/mongodb-atlas-cli/internal/config"
@@ -57,7 +58,10 @@ func DescribeBuilder() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "describe",
 		Aliases: []string{"get"},
-		Short:   "Get a maintenance window.",
+		Short:   "Return the maintenance window details for your project.",
+		Long:    `To learn more about maintenance windows, see https://www.mongodb.com/docs/atlas/tutorial/cluster-maintenance-window/.`,
+		Example: fmt.Sprintf(`  # Return the maintenance window for the project with the ID 5e2211c17a3e5a48f5497de3:
+  %s maintenanceWindows describe --projectId 5e2211c17a3e5a48f5497de3 --output json`, cli.ExampleAtlasEntryPoint()),
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			return opts.PreRunE(
 				opts.ValidateProjectID,

08aa5e3e22b8ad6d7850d6c655d06569edf9023f
Andrei Matei <andrei.matei@mongodb.com>
2023-01-26 20:42:43 +0000

    Fix misspell

--- internal/flag/flags.go
+++ internal/flag/flags.go
@@ -58,7 +58,7 @@ const (
 	Backup                                    = "backup"                                    // Backup flag
 	BIConnector                               = "biConnector"                               // BIConnector flag
 	ID                                        = "id"                                        // ID flag
-	EnableTerminationProtection               = "enabletTerminationProtection"              // EnableTerminationProtection flag
+	EnableTerminationProtection               = "enableTerminationProtection"               // EnableTerminationProtection flag
 	Username                                  = "username"                                  // Username flag
 	StorageEngine                             = "storageEngine"                             // StorageEngine flag
 	AuthMechanism                             = "authMechanism"                             // AuthMechanism flag

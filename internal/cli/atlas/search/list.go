9267bd529423e4ad5910ba4331a781f25a6b6ea1
Swati Tyagi <swati.tyagi@mongodb.com>
2023-03-30 16:46:14 +0100

    fix method signature

--- internal/cli/atlas/search/list.go
+++ internal/cli/atlas/search/list.go
@@ -50,7 +50,7 @@ var listTemplate = `ID	NAME	DATABASE	COLLECTION{{range .}}
 `
 
 func (opts *ListOpts) Run() error {
-	r, err := opts.store.SearchIndexes(opts.ConfigProjectID(), opts.clusterName, opts.dbName, opts.collName, opts.NewListOptions())
+	r, err := opts.store.SearchIndexes(opts.ConfigProjectID(), opts.clusterName, opts.dbName, opts.collName)
 	if err != nil {
 		return err
 	}

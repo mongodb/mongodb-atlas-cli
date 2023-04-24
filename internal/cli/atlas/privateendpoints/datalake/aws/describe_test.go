d3911d490683a841466c97b9008ff3f0fcd04e58
Swati Tyagi <swati.tyagi@mongodb.com>
2023-04-24 22:50:21 +0100

    CLOUDP-167779: Migrate Private Endpoint Services

--- internal/cli/atlas/privateendpoints/datalake/aws/describe_test.go
+++ internal/cli/atlas/privateendpoints/datalake/aws/describe_test.go
@@ -24,14 +24,14 @@ import (
 	"github.com/mongodb/mongodb-atlas-cli/internal/mocks"
 	"github.com/mongodb/mongodb-atlas-cli/internal/test"
 	"github.com/stretchr/testify/assert"
-	"go.mongodb.org/atlas/mongodbatlas"
+	atlasv2 "go.mongodb.org/atlas/mongodbatlasv2"
 )
 
 func TestDescribe_Run(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	mockStore := mocks.NewMockDataLakePrivateEndpointDescriber(ctrl)
 
-	expected := &mongodbatlas.PrivateLinkEndpointDataLake{}
+	expected := &atlasv2.PrivateNetworkEndpointIdEntry{}
 
 	describeOpts := &DescribeOpts{
 		store: mockStore,

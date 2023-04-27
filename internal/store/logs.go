fe92e5ee3da95f384b9d1c0cc92fe61e6f8b8794
Swati Tyagi <swati.tyagi@mongodb.com>
2023-04-27 15:52:44 +0100

    erro check

--- internal/store/logs.go
+++ internal/store/logs.go
@@ -86,7 +86,9 @@ func (s *Store) DownloadLog(groupID, host, name string, out io.Writer, opts *atl
 		start, _ := strconv.ParseInt(opts.StartDate, 10, 64)
 		end, _ := strconv.ParseInt(opts.EndDate, 10, 64)
 		logs, _, err := s.clientv2.MonitoringAndLogsApi.GetHostLogs(s.ctx, groupID, host, name).StartDate(start).EndDate(end).Execute()
-		io.Copy(out, logs)
+		if logs != nil {
+			io.Copy(out, logs)
+		}
 		return err
 	default:
 		return fmt.Errorf("%w: %s", errUnsupportedService, s.service)

b8f3ebab11e79aa793e77f91043f8e4fec95dca5
Swati Tyagi <swati.tyagi@mongodb.com>
2023-04-28 11:14:19 +0100

    fix file issue

--- internal/store/logs.go
+++ internal/store/logs.go
@@ -17,7 +17,9 @@ package store
 import (
 	"fmt"
 	"io"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/mongodb/mongodb-atlas-cli/internal/config"
 	atlas "go.mongodb.org/atlas/mongodbatlas"
@@ -83,9 +85,19 @@ func (s *Store) Collect(groupID string, newLog *opsmngr.LogCollectionJob) (*opsm
 func (s *Store) DownloadLog(groupID, host, name string, out io.Writer, opts *atlas.DateRangetOptions) error {
 	switch s.service {
 	case config.CloudService, config.CloudGovService:
-		start, _ := strconv.ParseInt(opts.StartDate, 10, 64)
-		end, _ := strconv.ParseInt(opts.EndDate, 10, 64)
-		logs, _, err := s.clientv2.MonitoringAndLogsApi.GetHostLogs(s.ctx, groupID, host, name).StartDate(start).EndDate(end).Execute()
+		fileBaseName := strings.TrimSuffix(name, filepath.Ext(name))
+		result := s.clientv2.MonitoringAndLogsApi.GetHostLogs(s.ctx, groupID, host, fileBaseName)
+		if opts != nil {
+			if opts.StartDate != "" {
+				start, _ := strconv.ParseInt(opts.StartDate, 10, 64)
+				result = result.StartDate(start)
+			}
+			if opts.EndDate != "" {
+				end, _ := strconv.ParseInt(opts.EndDate, 10, 64)
+				result = result.EndDate(end)
+			}
+		}
+		logs, _, err := result.Execute()
 		_, _ = io.Copy(out, logs)
 		return err
 	default:

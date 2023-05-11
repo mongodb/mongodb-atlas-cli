07a490f695857964317855a5495b256ebdacdb3f
wtrocki <w.trocki@mongodb.com>
2023-05-11 15:40:51 +0200

    fix: initial imports fix

--- test/e2e/atlas/online_archives_test.go
+++ test/e2e/atlas/online_archives_test.go
@@ -25,7 +25,7 @@ import (
 
 	"github.com/mongodb/mongodb-atlas-cli/test/e2e"
 	"github.com/stretchr/testify/assert"
-	atlasv2 "go.mongodb.org/atlas/mongodbatlasv2"
+	atlasv2 "go.mongodb.org/atlas-sdk/admin"
 )
 
 func TestOnlineArchives(t *testing.T) {

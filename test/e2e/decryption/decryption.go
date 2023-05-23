0b1b80da8324a57fe0265123824610315366d49b
Ciprian Tibulca <ciprian.tibulca@mongodb.com>
2023-05-23 23:57:13 +0300

    Update test/e2e/decryption/decryption.go

--- test/e2e/decryption/decryption.go
+++ test/e2e/decryption/decryption.go
@@ -73,8 +73,8 @@ func LogsAreEqual(t *testing.T, expected, got []byte) {
 	require.NoError(t, err)
 
 	if diff := deep.Equal(expectedLines, gotLines); diff != nil {
-		t.Logf("expectedLines: %s\n", expected)
-		t.Logf("gotLines: %s\n", string(got))
+		t.Logf("=== expected audit lines ===\n%s\n=== end expected lines ===", expected)
+		t.Logf("=== actual audit lines ===\n%s\n=== end actual lines ===", string(got))
 		t.Error(diff)
 	}
 }

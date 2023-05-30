e0ce5cbb17cefe89c5a738ce282f524293e06c56
Swati Tyagi <swati.tyagi@mongodb.com>
2023-05-30 13:31:58 +0100

    docs generate

--- internal/cli/atlas/clusters/availableregions/list_test.go
+++ internal/cli/atlas/clusters/availableregions/list_test.go
@@ -17,14 +17,13 @@
 package availableregions
 
 import (
-	"go.mongodb.org/atlas-sdk/admin"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/mongodb/mongodb-atlas-cli/internal/flag"
 	"github.com/mongodb/mongodb-atlas-cli/internal/mocks"
-	"github.com/mongodb/mongodb-atlas-cli/internal/pointer"
 	"github.com/mongodb/mongodb-atlas-cli/internal/test"
+	"go.mongodb.org/atlas-sdk/admin"
 )
 
 func TestList_Run_NoFlags(t *testing.T) {

a24bbf56f1ff6b319b5d7450be51f29624885dc7
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-08 17:23:01 +0100

    CLOUDP-182875: Improve example for atlas customDbRoles create command

--- internal/cli/atlas/customdbroles/create.go
+++ internal/cli/atlas/customdbroles/create.go
@@ -84,10 +84,13 @@ func CreateBuilder() *cobra.Command {
 		Short: "Create a custom database role for your project.",
 		Long:  fmt.Sprintf(usage.RequiredRole, "Project Owner"),
 		Example: fmt.Sprintf(`# Create a custom database role
-  %[1]s customDbRoles create customRole --privilege FIND@database,UPDATE@database
+  %[1]s customDbRoles create customRole --privilege FIND@databaseName,UPDATE@databaseName.collectionName
+
+  # Create a customer database role with granted action on the cluster resource
+  %[1]s customDbRoles create customRole --privilege GET_CMD_LINE_OPTS
 
   # Use an inherited role
-  %[1]s customDbRoles create customRole --inheritedRole read@database`, cli.ExampleAtlasEntryPoint()),
+  %[1]s customDbRoles create customRole --inheritedRole read@databaseName`, cli.ExampleAtlasEntryPoint()),
 		Args: require.ExactArgs(1),
 		Annotations: map[string]string{
 			"roleNameDesc": "Name of the custom role to create.",

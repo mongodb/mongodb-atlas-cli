9dd2d148fa6cce19c5b5df00882502e3f59a0d01
Lander McFall Kerbey <lander.kerbey@mongodb.com>
2023-06-09 11:49:51 -0400

    (DOCSP-30263) Copy review.

--- internal/cli/atlas/kubernetes/config/generate.go
+++ internal/cli/atlas/kubernetes/config/generate.go
@@ -105,8 +105,8 @@ func GenerateBuilder() *cobra.Command {
 		Args:    require.NoArgs,
 		Aliases: cli.GenerateAliases(use),
 		Short:   "Generate Kubernetes configuration resources for use with Atlas Kubernetes Operator.",
-		Long: `This command exports configurations for Atlas objects including Projects, 
-		Deployments, and Users in a Kubernetes-compatible format, allowing you to manage these 
+		Long: `This command exports configurations for Atlas objects including projects, 
+		deployments, and users in a Kubernetes-compatible format, allowing you to manage these 
 		resources using the Atlas Kubernetes Operator. For more information, see https://www.mongodb.com/docs/atlas/atlas-operator/`,
 		Example: `# Export Project, DatabaseUsers resources for a specific project without connection and integration secrets:
   atlas kubernetes config generate --projectId=<projectId>

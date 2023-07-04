946f84ec5a8a6f85bf243bbfd902106c977b5901
Vincent Pikand <pikandvincent@gmail.com>
2023-07-04 10:20:48 +0100

    CLOUDP-187015: unhide `--projectId` flag

--- internal/cli/atlas/setup/setup_cmd.go
+++ internal/cli/atlas/setup/setup_cmd.go
@@ -196,9 +196,7 @@ func Builder() *cobra.Command {
 	cmd.Flags().BoolVar(&qsOpts.Confirm, flag.Force, false, usage.Force)
 	cmd.Flags().BoolVar(&qsOpts.CurrentIP, flag.CurrentIP, false, usage.CurrentIPSimplified)
 	cmd.Flags().StringToStringVar(&qsOpts.Tag, flag.Tag, nil, usage.Tag)
-
 	cmd.Flags().StringVar(&qsOpts.ProjectID, flag.ProjectID, "", usage.ProjectID)
-	_ = cmd.Flags().MarkHidden(flag.ProjectID)
 
 	cmd.MarkFlagsMutuallyExclusive(flag.CurrentIP, flag.AccessListIP)
 

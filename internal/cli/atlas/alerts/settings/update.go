1d731e0594937eceb6fb0bdf286672b13763cbec
pierwill <19642016+pierwill@users.noreply.github.com>
2023-07-11 05:02:09 -0500

    DOCSP-19019: Correct info in mongocli ops-manager alerts settings documentation (#1988)
    Co-authored-by: pierwill <pierwill@users.noreply.github.com>

--- internal/cli/atlas/alerts/settings/update.go
+++ internal/cli/atlas/alerts/settings/update.go
@@ -122,6 +122,7 @@ func UpdateBuilder() *cobra.Command {
 	cmd.Flags().StringVar(&opts.ProjectID, flag.ProjectID, "", usage.ProjectID)
 	cmd.Flags().StringVarP(&opts.Output, flag.Output, flag.OutputShort, "", usage.FormatOut)
 	_ = cmd.RegisterFlagCompletionFunc(flag.Output, opts.AutoCompleteOutputFlag())
+	_ = cmd.MarkFlagRequired(flag.Event)
 
 	return cmd
 }

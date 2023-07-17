6ff3364c636122423542205b27b8130c08e38b50
eforeman-mdb <elyse.foreman@mongodb.com>
2023-07-17 09:51:11 -0400

    DOCSP-27608 Added examples for atlas alerts setting enable/disable

--- internal/cli/atlas/alerts/settings/enable.go
+++ internal/cli/atlas/alerts/settings/enable.go
@@ -66,6 +66,8 @@ func EnableBuilder() *cobra.Command {
 				opts.InitOutput(cmd.OutOrStdout(), enableTemplate),
 			)
 		},
+		Example: fmt.Sprintf(`  # Enable the alert configuration with the ID 5d1113b25a115342acc2d1aa in the project with the ID 5e2211c17a3e5a48f5497de3:
+  %s alerts settings enable 5d1113b25a115342acc2d1aa --projectId 5e2211c17a3e5a48f5497de3`, cli.ExampleAtlasEntryPoint()),		
 		Annotations: map[string]string{
 			"alertConfigIdDesc": "ID of the alert you want to enable.",
 			"output":            enableTemplate,

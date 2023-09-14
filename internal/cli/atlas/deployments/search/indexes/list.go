806a090d5df4694f61b6c5a60edc3012e0cf426f
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-09-14 09:48:46 +0100

    fixed errors

---
+++ internal/cli/atlas/deployments/search/indexes/list.go
@@ -0,0 +1,160 @@
+// Copyright 2023 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package indexes
+
+import (
+	"context"
+	"errors"
+	"github.com/mongodb/mongodb-atlas-cli/internal/cli"
+	"github.com/mongodb/mongodb-atlas-cli/internal/cli/atlas/deployments/options"
+	"github.com/mongodb/mongodb-atlas-cli/internal/cli/atlas/search"
+	"github.com/mongodb/mongodb-atlas-cli/internal/cli/require"
+	"github.com/mongodb/mongodb-atlas-cli/internal/config"
+	"github.com/mongodb/mongodb-atlas-cli/internal/flag"
+	"github.com/mongodb/mongodb-atlas-cli/internal/log"
+	"github.com/mongodb/mongodb-atlas-cli/internal/mongodbclient"
+	"github.com/mongodb/mongodb-atlas-cli/internal/podman"
+	"github.com/mongodb/mongodb-atlas-cli/internal/store"
+	"github.com/mongodb/mongodb-atlas-cli/internal/usage"
+	"github.com/spf13/cobra"
+)
+
+var listTemplate = `ID	NAME	DATABASE	COLLECTION
+{{.ID}}	{{.Name}}	{{.Database}}	{{.Collection}}
+`
+
+type ListOpts struct {
+	cli.GlobalOpts
+	cli.OutputOpts
+	options.DeploymentOpts
+	search.IndexOpts
+	mongodbClient mongodbclient.MongoDBClient
+	store         store.SearchIndexDescriber
+}
+
+func (opts *ListOpts) Run(ctx context.Context) error {
+	if err := opts.validateAndPrompt(ctx); err != nil {
+		return err
+	}
+
+	err := opts.RunLocal(ctx)
+	if err != nil && (errors.Is(err, options.ErrDeploymentNotFound) || errors.Is(err, mongodbclient.ErrSearchIndexNotFound)) {
+		return opts.RunAtlas()
+	}
+
+	return err
+}
+
+func (opts *ListOpts) RunAtlas() error {
+	r, err := opts.store.SearchIndex(opts.ConfigProjectID(), opts.DeploymentName, opts.indexID)
+	if err != nil {
+		return err
+	}
+
+	index := mongodbclient.SearchIndexDefinition{
+		ID:         *r.IndexID,
+		Name:       r.Name,
+		Collection: r.CollectionName,
+		Database:   r.Database,
+	}
+
+	return opts.Print(index)
+}
+
+func (opts *ListOpts) RunLocal(ctx context.Context) error {
+	connectionString, err := opts.ConnectionString(ctx)
+	if err != nil {
+		return err
+	}
+
+	if err = opts.mongodbClient.Connect(ctx, connectionString, connectWaitSeconds); err != nil {
+		return err
+	}
+	defer opts.mongodbClient.Disconnect(ctx)
+
+	r, err := opts.mongodbClient.SearchIndex(ctx, opts.indexID)
+	if err != nil {
+		return err
+	}
+
+	return opts.Print(r)
+}
+
+func (opts *ListOpts) initMongoDBClient() error {
+	opts.mongodbClient = mongodbclient.NewClient()
+	return nil
+}
+
+func (opts *ListOpts) initStore(ctx context.Context) func() error {
+	return func() error {
+		var err error
+		opts.store, err = store.New(store.AuthenticatedPreset(config.Default()), store.WithContext(ctx))
+		return err
+	}
+}
+
+func (opts *ListOpts) validateAndPrompt(ctx context.Context) error {
+	if opts.DBName == "" {
+		if err := promptRequiredName("Database", &opts.DBName); err != nil {
+			return err
+		}
+	}
+
+	if opts.Collection == "" {
+		if err := promptRequiredName("Collection", &opts.Collection); err != nil {
+			return err
+		}
+	}
+
+	if opts.DeploymentName == "" {
+		if err := opts.DeploymentOpts.Select(ctx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func ListBuilder() *cobra.Command {
+	opts := &ListOpts{}
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List all Atlas Search indexes for a deployment.",
+		Args:  require.NoArgs,
+		Annotations: map[string]string{
+			"output": listTemplate,
+		},
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			w := cmd.OutOrStdout()
+			opts.PodmanClient = podman.NewClient(log.IsDebugLevel(), w)
+			return opts.PreRunE(
+				opts.InitOutput(w, describeTemplate),
+				opts.InitStore(opts.PodmanClient),
+				opts.initStore(cmd.Context()),
+				opts.initMongoDBClient,
+			)
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return opts.Run(cmd.Context())
+		},
+	}
+
+	cmd.Flags().StringVar(&opts.DeploymentName, flag.ClusterName, "", usage.ClusterName)
+	cmd.Flags().StringVar(&opts.DBName, flag.Database, "", usage.Database)
+	cmd.Flags().StringVar(&opts.Collection, flag.Collection, "", usage.Collection)
+	cmd.Flags().StringVar(&opts.ProjectID, flag.ProjectID, "", usage.ProjectID)
+
+	return cmd
+}

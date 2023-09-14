c85074c1a0541091b8f95f54a0f707fde0861556
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-09-14 16:20:14 +0100

    Addressed Ciprain's comments

--- internal/cli/atlas/deployments/search/indexes/list.go
+++ internal/cli/atlas/deployments/search/indexes/list.go
@@ -30,7 +30,7 @@ import (
 	"github.com/mongodb/mongodb-atlas-cli/internal/store"
 	"github.com/mongodb/mongodb-atlas-cli/internal/usage"
 	"github.com/spf13/cobra"
-	atlasv2 "go.mongodb.org/atlas-sdk/v20230201007/admin"
+	atlasv2 "go.mongodb.org/atlas-sdk/v20230201008/admin"
 )
 
 var listTemplate = `ID	NAME	DATABASE	COLLECTION{{range .}}
@@ -139,9 +139,10 @@ func newSearchIndexDefinition(indexes []atlasv2.ClusterSearchIndex) []*mongodbcl
 func ListBuilder() *cobra.Command {
 	opts := &ListOpts{}
 	cmd := &cobra.Command{
-		Use:   "list",
-		Short: "List all Atlas Search indexes for a deployment.",
-		Args:  require.NoArgs,
+		Use:     "list",
+		Short:   "List all Atlas Search indexes for a deployment.",
+		Aliases: []string{"ls"},
+		Args:    require.NoArgs,
 		Annotations: map[string]string{
 			"output": listTemplate,
 		},

e6d1292bfe675edeaa145dc2e6b0cdc8020a64c5
Bianca <bianca.vianadeaguiar@mongodb.com>
2023-09-15 15:05:19 +0100

    Add alias to delete

--- internal/cli/atlas/deployments/delete.go
+++ internal/cli/atlas/deployments/delete.go
@@ -69,9 +69,10 @@ func DeleteBuilder() *cobra.Command {
 		DeleteOpts: cli.NewDeleteOpts("Deployment '%s' deleted\n", "Deployment not deleted"),
 	}
 	cmd := &cobra.Command{
-		Use:   "delete [deploymentName]",
-		Short: "Delete a deployment.",
-		Args:  require.MaximumNArgs(1),
+		Use:     "delete [deploymentName]",
+		Short:   "Delete a deployment.",
+		Aliases: []string{"rm"},
+		Args:    require.MaximumNArgs(1),
 		Annotations: map[string]string{
 			"deploymentNameDesc": "Name of the deployment you want to delete.",
 			"output":             opts.SuccessMessage(),

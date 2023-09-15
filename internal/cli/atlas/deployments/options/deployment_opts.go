b54f4148c7d7bf5e06e9a0435f0d1314237c3ca4
Filipe C Menezes <filipe.menezes@mongodb.com>
2023-09-15 16:06:01 +0100

    switch to preview

--- internal/cli/atlas/deployments/options/deployment_opts.go
+++ internal/cli/atlas/deployments/options/deployment_opts.go
@@ -33,7 +33,7 @@ const (
 	spinnerSpeed         = 100 * time.Millisecond
 	// based on https://www.mongodb.com/docs/atlas/reference/api-resources-spec/v2/#tag/Clusters/operation/createCluster
 	clusterNamePattern    = "^[a-zA-Z0-9][a-zA-Z0-9-]*$"
-	MongotDockerImageName = "docker.io/mongodb/mongodb-atlas-search:latest"
+	MongotDockerImageName = "docker.io/mongodb/mongodb-atlas-search:preview"
 )
 
 var (

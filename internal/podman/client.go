d8ba589a35e745568c487c9db7647d89b603e562
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-09-20 18:12:13 +0100

    e2e test

--- internal/podman/client.go
+++ internal/podman/client.go
@@ -341,6 +341,12 @@ func (o *client) RunContainer(ctx context.Context, opts RunContainerOpts) ([]byt
 		"--name", opts.Name,
 		"--hostname", opts.Hostname,
 		"--network", opts.Network,
+		"--cgroupns=host",
+
+		// Configure cgroup v2: sets the memory.high limit to 2GB
+		// podman defaults to cgroup v1 which does not support the container pause command in Linux
+		// https://github.com/containers/podman/issues/12782
+		// "--cgroup-conf=memory.high=2147483648",
 	}
 
 	for hostVolume, pathInContainer := range opts.Volumes {
@@ -378,7 +384,7 @@ func (o *client) RunContainer(ctx context.Context, opts RunContainerOpts) ([]byt
 	}
 
 	if runtime.GOOS == "linux" {
-		arg = append(arg, "--cgroup-manager=systemd")
+		arg = append(arg, "--cgroup-parent=/etc/subuid")
 	}
 
 	arg = append(arg, opts.Args...)

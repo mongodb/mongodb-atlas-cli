b4300ac233cc132073e2a40dab4810117356674a
blva <40155621+blva@users.noreply.github.com>
2023-09-21 15:52:07 +0100

    CLOUDP-197016: Add atlas deployments search index create (#2302)

--- test/e2e/atlas/deployments_local_test.go
+++ test/e2e/atlas/deployments_local_test.go
@@ -47,7 +47,7 @@ func splitOutput(cmd *exec.Cmd) (string, string, error) {
 	return o.String(), e.String(), err
 }
 
-func TestDeployments(t *testing.T) {
+func TestDeploymentsLocal(t *testing.T) {
 	g := newAtlasE2ETestGenerator(t)
 	g.generateProject("deployments")
 
@@ -169,6 +169,8 @@ test   LOCAL   7.0.1     IDLE
 			indexEntity,
 			"create",
 			indexName,
+			"--type",
+			"local",
 			"--deploymentName",
 			deploymentName,
 			"--db",

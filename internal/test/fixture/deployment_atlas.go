9f9cdb6f809321edc06972945dec70169508c31e
Bianca <bianca.vianadeaguiar@mongodb.com>
2023-10-10 16:55:58 +0100

    Add common test logic to fixture

---
+++ internal/test/fixture/deployment_atlas.go
@@ -0,0 +1,60 @@
+package fixture
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/mongodb/mongodb-atlas-cli/internal/cli/atlas/deployments/options"
+	"github.com/mongodb/mongodb-atlas-cli/internal/config"
+	"github.com/mongodb/mongodb-atlas-cli/internal/mocks"
+	"github.com/mongodb/mongodb-atlas-cli/internal/pointer"
+	"go.mongodb.org/atlas-sdk/v20230201008/admin"
+)
+
+func NewMockAtlasDeploymentOpts(ctrl *gomock.Controller, deploymentName string) MockDeploymentOpts {
+	mockCredentialsGetter := mocks.NewMockCredentialsGetter(ctrl)
+	mockAtlasClusterListStore := mocks.NewMockClusterLister(ctrl)
+
+	return MockDeploymentOpts{
+		ctrl:                      ctrl,
+		MockCredentialsGetter:     mockCredentialsGetter,
+		MockAtlasClusterListStore: mockAtlasClusterListStore,
+		Opts: &options.DeploymentOpts{
+			CredStore:             mockCredentialsGetter,
+			AtlasClusterListStore: mockAtlasClusterListStore,
+			DeploymentName:        deploymentName,
+			DeploymentType:        "atlas",
+		},
+	}
+}
+
+func (m *MockDeploymentOpts) MockPaginatedAdvancedClusterDescription() *admin.PaginatedAdvancedClusterDescription {
+	return &admin.PaginatedAdvancedClusterDescription{
+		Results: []admin.AdvancedClusterDescription{
+			{
+				Name:           pointer.Get(m.Opts.DeploymentName),
+				Id:             pointer.Get("123"),
+				MongoDBVersion: pointer.Get("7.0.0"),
+				StateName:      pointer.Get("IDLE"),
+				Paused:         pointer.Get(false),
+			},
+		},
+	}
+}
+
+func (m *MockDeploymentOpts) CommonAtlasMocks(projectID string) {
+	m.MockCredentialsGetter.
+		EXPECT().
+		AuthType().
+		Return(config.OAuth).
+		Times(1)
+
+	m.MockAtlasClusterListStore.
+		EXPECT().
+		ProjectClusters(projectID, gomock.Any()).
+		Return(m.MockPaginatedAdvancedClusterDescription(), nil).
+		Times(1)
+
+}
+
+func (m *MockDeploymentOpts) localMockFlow(projectID string) {
+	return
+}
---
+++ internal/test/fixture/deployment_local.go
@@ -0,0 +1,52 @@
+package fixture
+
+import (
+	"context"
+
+	"github.com/golang/mock/gomock"
+	"github.com/mongodb/mongodb-atlas-cli/internal/cli/atlas/deployments/options"
+	"github.com/mongodb/mongodb-atlas-cli/internal/mocks"
+	"github.com/mongodb/mongodb-atlas-cli/internal/podman"
+)
+
+func NewMockLocalDeploymentOpts(ctrl *gomock.Controller, deploymentName string) MockDeploymentOpts {
+	mockPodman := mocks.NewMockClient(ctrl)
+	return MockDeploymentOpts{
+		ctrl:       ctrl,
+		MockPodman: mockPodman,
+		Opts: &options.DeploymentOpts{
+			PodmanClient:   mockPodman,
+			DeploymentName: deploymentName,
+			DeploymentType: "local",
+		},
+	}
+}
+func (m *MockDeploymentOpts) LocalMockFlow(ctx context.Context) {
+	m.MockPodman.
+		EXPECT().
+		Ready(ctx).
+		Return(nil).
+		Times(1)
+
+	m.MockPodman.
+		EXPECT().
+		ListContainers(ctx, options.MongodHostnamePrefix).
+		Return(m.MockContainer(), nil).
+		Times(1)
+
+}
+
+func (m *MockDeploymentOpts) MockContainer() []*podman.Container {
+	return m.MockContainerWithState("running")
+}
+
+func (m *MockDeploymentOpts) MockContainerWithState(state string) []*podman.Container {
+	return []*podman.Container{
+		{
+			Names:  []string{m.Opts.DeploymentName},
+			State:  state,
+			Labels: map[string]string{"version": "6.0.9"},
+			ID:     m.Opts.DeploymentName,
+		},
+	}
+}

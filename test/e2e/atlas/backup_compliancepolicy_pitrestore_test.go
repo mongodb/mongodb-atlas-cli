29e47b97bfea6ac6d352c786e278ec048cac3dfc
Filipe C Menezes <filipe.menezes@mongodb.com>
2023-11-02 10:45:36 +0000

    fix lint

--- test/e2e/atlas/backup_compliancepolicy_pitrestore_test.go
+++ test/e2e/atlas/backup_compliancepolicy_pitrestore_test.go
@@ -29,7 +29,7 @@ import (
 )
 
 func TestBackupCompliancePolicyPointInTimeRestore(t *testing.T) {
-	//TODO remove after CLOUDP-198381
+	// TODO remove after CLOUDP-198381
 	t.Skip("reenable tests when CLOUDP-198381 is addressed")
 
 	cliPath, err := e2e.AtlasCLIBin()

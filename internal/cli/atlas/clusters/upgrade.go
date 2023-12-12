00e34cc2c5223ef0af442993a84284ade146eb52
Gustavo Bazan <gus.bazan@hey.com>
2023-12-12 17:20:51 -0500

    Update upgrade.go

--- internal/cli/atlas/clusters/upgrade.go
+++ internal/cli/atlas/clusters/upgrade.go
@@ -133,7 +133,7 @@ func UpgradeBuilder() *cobra.Command {
 
 ` + fmt.Sprintf(usage.RequiredRole, "Project Cluster Manager"),
 		Example: fmt.Sprintf(`  # Upgrade the tier, disk size, and MongoDB version for the shared cluster named myCluster in the project with the ID 5e2211c17a3e5a48f5497de3:
-  %s cluster upgrade myCluster --projectId 5e2211c17a3e5a48f5497de3 --tier M50 --diskSizeGB 20 --mdbVersion 4.2 --tag env=dev`,
+  %s cluster upgrade myCluster --projectId 5e2211c17a3e5a48f5497de3 --tier M50 --diskSizeGB 20 --mdbVersion 7.0 --tag env=dev`,
 			cli.ExampleAtlasEntryPoint()),
 		Args: require.ExactArgs(1),
 		PreRunE: func(cmd *cobra.Command, args []string) error {

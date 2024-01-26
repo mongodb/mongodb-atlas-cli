6d83cbdc96b337bd1bbdc117911316026313910b
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-01-26 18:08:36 +0000

    test22

--- build/package/linux_notarize.sh
+++ build/package/linux_notarize.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 #set -Eeou pipefail
 #
 ## Notarize generated binaries with GPG and replace the original binary with the notarized one

485f5293ce67ebc0c5c70499f6a4d480ffcaeb18
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-01-29 10:17:52 +0000

    test22

--- build/package/linux_notarize2.sh
+++ build/package/linux_notarize2.sh
@@ -26,7 +26,7 @@ if [[ -f "${LINUX_FILE}" ]]; then
   echo "YESSSSSSSSSSS here2"
   echo "${ARTIFACTORY_PASSWORD}" | docker login --password-stdin --username "${ARTIFACTORY_USERNAME}" artifactory.corp.mongodb.com
 
-	echo "notarizing Linux binary ${AMD64_TAR_FILE}"
+	echo "notarizing Linux binary ${LINUX_FILE}"
   docker run \
     -e GRS_CONFIG_USER1_USERNAME="${GRS_USERNAME}" \
     -e GRS_CONFIG_USER1_PASSWORD="${GRS_PASSWORD}" \

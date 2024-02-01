99f8ff4e612859e4c50f6b4da31773af780b605b
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-02-01 10:59:40 +0000

    testing-to remove 1

--- cmd/atlas/atlas.go
+++ cmd/atlas/atlas.go
@@ -24,10 +24,10 @@ import (
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/AlecAivazis/survey/v2/core"
-	"github.com/mongodb/mongodb-atlas-cli/internal/cli/root/atlas"
-	"github.com/mongodb/mongodb-atlas-cli/internal/config"
-	"github.com/mongodb/mongodb-atlas-cli/internal/telemetry"
-	"github.com/mongodb/mongodb-atlas-cli/internal/terminal"
+	"github.com/andreangiolillo/mongodbcli-test/internal/cli/root/atlas"
+	"github.com/andreangiolillo/mongodbcli-test/internal/config"
+	"github.com/andreangiolillo/mongodbcli-test/internal/telemetry"
+	"github.com/andreangiolillo/mongodbcli-test/internal/terminal"
 	"github.com/spf13/cobra"
 )
 

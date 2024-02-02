90f1068761cb4419f0f2bdd6aed524d7a9563c5f
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-02-02 13:41:47 +0000

    t

--- test/e2e/atlas/deployments_local_auth_test.go
+++ test/e2e/atlas/deployments_local_auth_test.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//go:build e2e || (atlas && deployments && local && auth)
+//go:build e2e || (atlas && deployments && auth && local)
 
 package atlas_test
 

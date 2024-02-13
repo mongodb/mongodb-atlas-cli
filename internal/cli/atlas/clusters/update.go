7b4c9ebd6a8f35d397b08b5c6e02f675e51e44ae
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2024-02-13 14:03:29 +0000

    CLOUDP-228051: [Atlas CLI] atlas cluster update via config file fails with "HTTP 400 Bad Request (Error code: "INVALID_JSON_ATTRIBUTE") (#2628)

--- internal/cli/atlas/clusters/update.go
+++ internal/cli/atlas/clusters/update.go
@@ -63,6 +63,9 @@ func (opts *UpdateOpts) Run() error {
 	if err != nil {
 		return err
 	}
+
+	removeReadOnlyAttributes(cluster)
+
 	if opts.filename == "" {
 		opts.patchOpts(cluster)
 	}
@@ -85,13 +88,18 @@ func (opts *UpdateOpts) cluster() (*atlasv2.AdvancedClusterDescription, error) {
 		if opts.name == "" {
 			opts.name = cluster.GetName()
 		}
+
+		// The cluster name cannot be updated by the Update operation
+		if cluster.GetName() != "" && opts.name != cluster.GetName() {
+			cluster.Name = nil
+		}
+
 		return cluster, nil
 	}
 	return opts.store.AtlasCluster(opts.ProjectID, opts.name)
 }
 
 func (opts *UpdateOpts) patchOpts(out *atlasv2.AdvancedClusterDescription) {
-	removeReadOnlyAttributes(out)
 	if opts.mdbVersion != "" {
 		out.MongoDBMajorVersion = &opts.mdbVersion
 	}

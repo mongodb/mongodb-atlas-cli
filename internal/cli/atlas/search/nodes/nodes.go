ca8f686e4b4d873e585ec167fa4cc486b41ea8cc
Jeroen Vervaeke <9132134+jeroenvervaeke@users.noreply.github.com>
2024-02-13 17:03:36 +0000

    [CLOUDP-229226] Search node management - implement list command (#2629)
    Co-authored-by: Bianca Lisle <40155621+blva@users.noreply.github.com>
    Co-authored-by: Gustavo Bazan <gustavo.bazan@mongodb.com>

--- internal/cli/atlas/search/nodes/nodes.go
+++ internal/cli/atlas/search/nodes/nodes.go
@@ -1,10 +1,10 @@
-// Copyright 2020 MongoDB Inc
+// Copyright 2024 MongoDB Inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,23 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package search
+package nodes
 
 import (
-	"github.com/mongodb/mongodb-atlas-cli/internal/cli/atlas/search/nodes"
+	"github.com/mongodb/mongodb-atlas-cli/internal/cli"
 	"github.com/spf13/cobra"
 )
 
 func Builder() *cobra.Command {
+	const use = "nodes"
 	cmd := &cobra.Command{
-		Use:     "search",
-		Aliases: []string{"fts"},
-		Short:   "Manage Atlas Search for your cluster.",
+		Use:     use,
+		Aliases: cli.GenerateAliases(use),
+		Short:   "Manage Atlas Search nodes for your cluster.",
 	}
 	cmd.AddCommand(
-		IndexesBuilder(),
-		nodes.Builder(),
+		ListBuilder(),
 	)
-
 	return cmd
 }

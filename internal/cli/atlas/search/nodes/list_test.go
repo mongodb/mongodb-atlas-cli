efc449bc28c2e7073441a18997714fcfbfe06026
Jeroen Vervaeke <jeroen.vervaeke@mongodb.com>
2024-02-14 14:47:36 +0000

    Added unit test for list builder

--- internal/cli/atlas/search/nodes/list_test.go
+++ internal/cli/atlas/search/nodes/list_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/mongodb/mongodb-atlas-cli/internal/flag"
 	"github.com/mongodb/mongodb-atlas-cli/internal/mocks"
 	"github.com/mongodb/mongodb-atlas-cli/internal/pointer"
 	"github.com/mongodb/mongodb-atlas-cli/internal/test"
@@ -54,4 +55,16 @@ func TestList_Run(t *testing.T) {
 		t.Fatalf("Run() unexpected error: %v", err)
 	}
 	test.VerifyOutputTemplate(t, listTemplate, expected)
+}
+
+func TestBuilder(t *testing.T) {
+	test.CmdValidator(
+		t,
+		ListBuilder(),
+		0,
+		[]string{
+			flag.ClusterName,
+			flag.ProjectID,
+		},
+	)
 }

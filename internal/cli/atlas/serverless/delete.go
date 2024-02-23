af3748394687e636cd820c33fa4c3440dcc0078e
Gustavo Bazan <gus.bazan@hey.com>
2024-02-23 11:53:35 +0000

    CLOUDP-210224: More detailed message when deleting an instance

--- internal/cli/atlas/serverless/delete.go
+++ internal/cli/atlas/serverless/delete.go
@@ -65,7 +65,7 @@ func DeleteBuilder() *cobra.Command {
 				return err
 			}
 			opts.Entry = args[0]
-			return opts.Prompt()
+			return opts.PromptWithMessage("This operation will delete the instance, all of its data, and any associated backups.\nAre you sure you want to terminate %s, this action cannot be undone?")
 		},
 		RunE: func(_ *cobra.Command, _ []string) error {
 			return opts.Run()

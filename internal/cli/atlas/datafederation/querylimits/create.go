cf6d922303ecf83e1c4d9e2e15a6f7819e2d0906
Gustavo Bazan <gus.bazan@hey.com>
2024-03-08 17:24:26 +0000

    task: store clean up final

--- internal/cli/atlas/datafederation/querylimits/create.go
+++ internal/cli/atlas/datafederation/querylimits/create.go
@@ -24,7 +24,7 @@ import (
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/cli/require"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/config"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/flag"
-	store "github.com/mongodb/mongodb-atlas-cli/atlascli/internal/store/atlas"
+	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/store"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/usage"
 	"github.com/spf13/cobra"
 	"go.mongodb.org/atlas-sdk/v20231115007/admin"

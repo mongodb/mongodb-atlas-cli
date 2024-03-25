0363088404a8846e4a1788c26731dc6e6e5c71ec
Bianca <bianca.vianadeaguiar@mongodb.com>
2024-03-25 18:24:07 +0000

    Remove warnings

--- internal/cli/deployments/options/deployment_opts_telemetry.go
+++ internal/cli/deployments/options/deployment_opts_telemetry.go
@@ -15,7 +15,6 @@
 package options
 
 import (
-	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/log"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/telemetry"
 )
 
@@ -35,7 +34,6 @@ func (opts *DeploymentOpts) AppendDeploymentType() {
 	} else if opts.IsAtlasDeploymentType() {
 		deploymentType = AtlasCluster
 	}
-	log.Warning(deploymentType)
 	if deploymentType != "" {
 		telemetry.AppendOption(telemetry.WithDeploymentType(deploymentType))
 	}

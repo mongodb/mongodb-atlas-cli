1a8b5d4694a0dfae85e23278b61ad727e70f5172
Bianca Lisle <40155621+blva@users.noreply.github.com>
2024-04-25 17:13:22 +0100

    Update internal/cli/federatedauthentication/federationsettings/connectedorgsconfigs/disconnect.go
    Co-authored-by: John Williams <55147273+jwilliams-mongo@users.noreply.github.com>

--- internal/cli/federatedauthentication/federationsettings/connectedorgsconfigs/disconnect.go
+++ internal/cli/federatedauthentication/federationsettings/connectedorgsconfigs/disconnect.go
@@ -113,7 +113,7 @@ func DisconnectBuilder() *cobra.Command {
 		Annotations: map[string]string{
 			"output": disconnectTemplate,
 		},
-		Example: `  # Disconnect the current profile Org with Identity Provider from ID 5d1113b25a115342acc2d1aa and federationSettingsId 5d1113b25a115342acc2d1aa 
+		Example: `  # Disconnect the current profile org from identity provider with ID 5d1113b25a115342acc2d1aa and federationSettingsId 5d1113b25a115342acc2d1aa 
 			atlas federatedAuthentication connectedOrgs disconnect --identityProviderId 5d1113b25a115342acc2d1aa --federationSettingsId 5d1113b25a115342acc2d1aa 
 			# Connect the org with ID 7d1113b25a115342acc2d1aa to identity provider with ID 5d1113b25a115342acc2d1aa and federationSettingsId 5d1113b25a115342acc2d1aa 
 			atlas federatedAuthentication connectedOrgs disconnect --orgId 7d1113b25a115342acc2d1aa --identityProviderId 5d1113b25a115342acc2d1aa --federationSettingsId 5d1113b25a115342acc2d1aa 

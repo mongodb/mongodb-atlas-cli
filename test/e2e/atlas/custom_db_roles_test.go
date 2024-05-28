f635e9b50d0196ad4b691a915490d172fe541d88
Bianca <bianca.vianadeaguiar@mongodb.com>
2024-05-28 19:01:55 +0100

    Fix test

--- test/e2e/atlas/custom_db_roles_test.go
+++ test/e2e/atlas/custom_db_roles_test.go
@@ -30,6 +30,7 @@ import (
 
 const (
 	createPrivilege             = "UPDATE"
+	findPrivilege               = "FIND"
 	updatePrivilege             = "LIST_SESSIONS"
 	enableShardingRole          = "enableSharding"
 	enableShardingInheritedRole = "enableSharding@admin"
@@ -51,7 +52,7 @@ func TestDBRoles(t *testing.T) {
 			customDBRoleEntity,
 			"create",
 			roleName,
-			"--privilege", fmt.Sprintf("%s@db.collection,%s@db.collection2", createPrivilege, updatePrivilege),
+			"--privilege", fmt.Sprintf("%s@db.collection,%s@db.collection2", createPrivilege, findPrivilege),
 			"--inheritedRole", enableShardingInheritedRole,
 			"-o=json",
 		)
@@ -125,10 +126,10 @@ func TestDBRoles(t *testing.T) {
 
 		a := assert.New(t)
 		a.Equal(roleName, role.RoleName)
-		a.Len(role.GetActions(), 2)
+		a.Len(role.GetActions(), 3)
 		a.ElementsMatch(
 			[]string{role.GetActions()[0].Action, role.GetActions()[1].Action},
-			[]string{updatePrivilege, createPrivilege})
+			[]string{updatePrivilege, createPrivilege, findPrivilege})
 		a.ElementsMatch(
 			[]string{enableShardingRole, readRole},
 			[]string{role.GetInheritedRoles()[0].Role, role.GetInheritedRoles()[1].Role})

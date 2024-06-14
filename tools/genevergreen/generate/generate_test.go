da23edd8b7b40fd7799e696446e9c65b95d3e15c
Bianca Lisle <40155621+blva@users.noreply.github.com>
2024-06-14 10:44:34 +0100

    fix: Add MDB 8.0 (#3011)

--- tools/genevergreen/generate/generate_test.go
+++ tools/genevergreen/generate/generate_test.go
@@ -33,6 +33,6 @@ func TestPublishSnapshotTasks(t *testing.T) {
 func TestPublishStableTasks(t *testing.T) {
 	c := &shrub.Configuration{}
 	PublishStableTasks(c)
-	assert.Len(t, c.Variants, 3)
-	assert.Len(t, c.Tasks, 126)
+	assert.Len(t, c.Variants, 4)
+	assert.Len(t, c.Tasks, 168)
 }

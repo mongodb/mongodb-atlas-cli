e92f3a40a2977e234e6ef90ca9d190f1fae6fba6
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-06-18 15:01:14 +0100

    Added unit tests

--- internal/cli/auditing/update.go
+++ internal/cli/auditing/update.go
@@ -21,10 +21,13 @@ import (
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/cli"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/cli/require"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/config"
+	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/file"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/flag"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/store"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/usage"
+	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
+	atlasv2 "go.mongodb.org/atlas-sdk/v20231115014/admin"
 )
 
 type UpdateOpts struct {
@@ -34,7 +37,8 @@ type UpdateOpts struct {
 	auditAuthorizationSuccess bool
 	filename                  string
 	auditFilter               string
-	store                     store.AuditingDescriber
+	fs                        afero.Fs
+	store                     store.AuditingUpdater
 }
 
 func (opts *UpdateOpts) initStore(ctx context.Context) func() error {
@@ -45,23 +49,41 @@ func (opts *UpdateOpts) initStore(ctx context.Context) func() error {
 	}
 }
 
-var updateTemplate = `AUDIT AUTHORIZATION SUCCESS	AUDIT FILTER	CONFIGURATION TYPE	ENABLED
-{{.AuditAuthorizationSuccess}}	{{.AuditFilter}}	{{.ConfigurationType}}	{{.Enabled}}
-`
+var updateTemplate = "Auditing configuration successfully updated.\n"
 
 func (opts *UpdateOpts) Run() error {
-	print(opts.auditFilter)
-
-	r, err := opts.store.Auditing(opts.ConfigProjectID())
+	auditLog, err := opts.newAuditLog()
+	if err != nil {
+		return err
+	}
+	r, err := opts.store.UpdateAuditingConfig(opts.ConfigProjectID(), auditLog)
 	if err != nil {
 		return err
 	}
 	return opts.Print(r)
 }
 
+func (opts *UpdateOpts) newAuditLog() (*atlasv2.AuditLog, error) {
+	if opts.filename != "" {
+		fileContent, err := file.LoadFile(opts.fs, opts.filename)
+		if err != nil {
+			return nil, err
+		}
+		opts.auditFilter = string(fileContent)
+	}
+
+	return &atlasv2.AuditLog{
+		Enabled:                   &opts.enabled,
+		AuditAuthorizationSuccess: &opts.auditAuthorizationSuccess,
+		AuditFilter:               &opts.auditFilter,
+	}, nil
+}
+
 // UpdateBuilder atlas auditing update [--file filter.json] [--auditFilter "{"atype": "authenticate"}"] [--auditAuthorizationSuccess] [--enabled] --projectId projectId.
 func UpdateBuilder() *cobra.Command {
-	opts := &UpdateOpts{}
+	opts := &UpdateOpts{
+		fs: afero.NewOsFs(),
+	}
 	cmd := &cobra.Command{
 		Use:   "update",
 		Short: "Updates the auditing configuration for the specified project",
@@ -80,7 +102,7 @@ func UpdateBuilder() *cobra.Command {
 			return opts.PreRunE(
 				opts.ValidateProjectID,
 				opts.initStore(cmd.Context()),
-				opts.InitOutput(cmd.OutOrStdout(), describeTemplate),
+				opts.InitOutput(cmd.OutOrStdout(), updateTemplate),
 			)
 		},
 		RunE: func(_ *cobra.Command, _ []string) error {
@@ -93,6 +115,7 @@ func UpdateBuilder() *cobra.Command {
 	cmd.Flags().BoolVar(&opts.enabled, flag.Enabled, false, usage.EnabledAuditing)
 	cmd.Flags().StringVarP(&opts.filename, flag.File, flag.FileShort, "", usage.AuditingFilename)
 	cmd.Flags().BoolVar(&opts.auditAuthorizationSuccess, flag.AuditAuthorizationSuccess, false, usage.AuditAuthorizationSuccess)
+	cmd.Flags().StringVarP(&opts.Output, flag.Output, flag.OutputShort, "", usage.FormatOut)
 	_ = cmd.RegisterFlagCompletionFunc(flag.Output, opts.AutoCompleteOutputFlag())
 
 	_ = cmd.MarkFlagFilename(flag.File)

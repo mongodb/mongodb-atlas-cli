ec5bf7a1b1586fdcf755a30a664640c86c57c5ab
Stefan Humpelstetter <stefan.humpelstetter@mongodb.com>
2024-06-28 13:57:27 +0100

    Basic plugin functionality

--- internal/cli/root/builder.go
+++ internal/cli/root/builder.go
@@ -66,6 +66,7 @@ import (
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/homebrew"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/latestrelease"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/log"
+	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/plugin"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/prerun"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/sighandle"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/telemetry"
@@ -246,6 +247,12 @@ Use the --help flag with any command for more info on that command.`,
 		deployments.Builder(),
 		federatedauthentication.Builder(),
 	)
+
+	if commands, err := plugin.GetPluginCommands(); err != nil {
+		log.Warningf("Could not load plugins: %s", err.Error())
+	} else {
+		rootCmd.AddCommand(commands...)
+	}
 
 	rootCmd.PersistentFlags().StringVarP(&profile, flag.Profile, flag.ProfileShort, "", usage.ProfileAtlasCLI)
 	rootCmd.PersistentFlags().BoolVarP(&debugLevel, flag.Debug, flag.DebugShort, false, usage.Debug)

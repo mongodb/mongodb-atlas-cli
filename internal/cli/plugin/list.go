efb08097818f5677bff42d6a4f6a907f58556f7b
Stefan Humpelstetter <38726293+stefan4h@users.noreply.github.com>
2024-07-12 14:19:38 +0100

    CLOUDP-259871: Plugin discovery logic and plugin list command (#3098)
    Co-authored-by: Filipe Constantinov Menezes <filipe.menezes@mongodb.com>

---
+++ internal/cli/plugin/list.go
@@ -0,0 +1,57 @@
+// Copyright 2024 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plugin
+
+import (
+	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/cli"
+	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/plugin"
+	"github.com/spf13/cobra"
+)
+
+type ListOps struct {
+	cli.OutputOpts
+	plugins []*plugin.Plugin
+}
+
+func (opts *ListOps) Run() error {
+	return opts.Print(opts.plugins)
+}
+
+const listTemplate = `NAME	DESCRIPTION	VERSION {{range valueOrEmptySlice .}}
+{{.Name}}	{{.Description}}	{{.Version}}	COMMANDS: {{range valueOrEmptySlice .Commands}}
+		{{.Name}}{{end}}
+		{{end}}
+`
+
+func ListBuilder(plugins []*plugin.Plugin) *cobra.Command {
+	opts := &ListOps{
+		plugins: plugins,
+	}
+	const use = "list"
+	cmd := &cobra.Command{
+		Use:     use,
+		Aliases: cli.GenerateAliases(use),
+		Short:   "Returns a list of all installed plugins.",
+		PreRun: func(cmd *cobra.Command, _ []string) {
+			opts.OutWriter = cmd.OutOrStdout()
+			opts.Template = listTemplate
+		},
+		RunE: func(_ *cobra.Command, _ []string) error {
+			return opts.Run()
+		},
+	}
+
+	return cmd
+}
---
+++ internal/plugin/plugin_test.go
@@ -0,0 +1,62 @@
+// Copyright 2024 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build unit
+
+package plugin
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func getTestManifest() *Manifest {
+	return &Manifest{
+		Name:        "testPlugin",
+		Description: "Test plugin description",
+		BinaryPath:  "/plugins/testPlugin/binary",
+		Version:     "1.2.3",
+		Commands: map[string]ManifestCommand{
+			"testCommand": {"Test command"},
+		},
+	}
+}
+
+func Test_GetCobraCommands(t *testing.T) {
+	manifest := getTestManifest()
+
+	// Create a mock Plugin from the mock Manifest
+	plugin := createPluginFromManifest(manifest)
+
+	commands := plugin.GetCobraCommands()
+
+	assert.Len(t, commands, 1)
+	assert.Equal(t, "testCommand", commands[0].Use)
+	assert.Equal(t, manifest.Commands["testCommand"].Description, commands[0].Short)
+	assert.NotNil(t, commands[0].RunE)
+}
+
+func Test_createPluginFromManifest(t *testing.T) {
+	manifest := getTestManifest()
+
+	plugin := createPluginFromManifest(manifest)
+
+	assert.Equal(t, plugin.Name, manifest.Name)
+	assert.Equal(t, plugin.Description, manifest.Description)
+	assert.Equal(t, plugin.BinaryPath, manifest.BinaryPath)
+	assert.Equal(t, plugin.Version, manifest.Version)
+
+	assert.Len(t, plugin.Commands, len(manifest.Commands))
+}

547d80f3051745fc3582fe215ee90532105b0bf3
Stefan Humpelstetter <stefan.humpelstetter@mongodb.com>
2024-07-23 16:46:45 +0100

    unit test for Test_getAssetID

--- internal/cli/plugin/plugin_asset_test.go
+++ internal/cli/plugin/plugin_asset_test.go
@@ -17,8 +17,11 @@
 package plugin
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 
+	"github.com/google/go-github/v61/github"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -26,4 +29,68 @@ func Test_fullRepositoryDefinition(t *testing.T) {
 	opts := &AssetOpts{repositoryOwner: "repoOwner", repositoryName: "repoName"}
 
 	assert.Equal(t, opts.repositoryOwner+"/"+opts.repositoryName, opts.fullRepositoryDefinition())
+}
+
+func Test_getAssetID(t *testing.T) {
+	validAssetName := fmt.Sprintf("plugin_%s_%s", runtime.GOOS, runtime.GOARCH)
+
+	tests := []struct {
+		name            string
+		pluginAssets    []*github.ReleaseAsset
+		expectedAssetID int64
+		expectError     bool
+	}{
+		{
+			name: "Valid asset",
+			pluginAssets: []*github.ReleaseAsset{
+				{
+					ID:          github.Int64(1),
+					Name:        github.String(validAssetName + ".tar.gz"),
+					ContentType: github.String("application/gzip"),
+				},
+			},
+			expectedAssetID: 1,
+			expectError:     false,
+		},
+		{
+			name: "No matching asset",
+			pluginAssets: []*github.ReleaseAsset{
+				{
+					ID:          github.Int64(2),
+					Name:        github.String("plugin_invalid_assetname.tar.gz"),
+					ContentType: github.String("application/gzip"),
+				},
+			},
+			expectedAssetID: 0,
+			expectError:     true,
+		},
+		{
+			name: "Non-gzip asset",
+			pluginAssets: []*github.ReleaseAsset{
+				{
+					ID:          github.Int64(3),
+					Name:        github.String(validAssetName + ".zip"),
+					ContentType: github.String("application/zip"),
+				},
+			},
+			expectedAssetID: 0,
+			expectError:     true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opts := &AssetOpts{
+				pluginAssets: tt.pluginAssets,
+			}
+
+			assetID, err := opts.getAssetID()
+			if (err != nil) != tt.expectError {
+				t.Errorf("expected error: %v, got: %v", tt.expectError, err)
+			}
+			if assetID != tt.expectedAssetID {
+				t.Errorf("expected asset ID: %d, got: %d", tt.expectedAssetID, assetID)
+			}
+		})
+	}
 }

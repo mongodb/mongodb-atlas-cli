6bc657c63705501b2351d035c547c4f8267f1ed2
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-09 15:38:27 +0100

    CLOUDP-265393 Create Image job to Build Windows VM Image

---
+++ build/ci/packer/setup2.ps1
@@ -0,0 +1,15 @@
+Write-Output 'Sysprep...'
+if( Test-Path $Env:SystemRoot\windows\system32\Sysprep\unattend.xml ){
+    rm $Env:SystemRoot\windows\system32\Sysprep\unattend.xml -Force
+}
+& $Env:SystemRoot\System32\Sysprep\Sysprep.exe /oobe /generalize /quit /quiet /mode:vm
+while($true) {
+    $imageState = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State | Select ImageState
+    Write-Output $imageState.ImageState
+    if($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') {
+        Start-Sleep -s 10 
+    } else { 
+        break
+    }
+}
+Write-Output 'Done'

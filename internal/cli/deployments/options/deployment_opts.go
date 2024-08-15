64fc2f01e94c5141b718e6c1c339c7c90470ab4e
Jeroen Vervaeke <jeroen.vervaeke@mongodb.com>
2024-08-15 15:41:02 +0100

    Added minimum cpu count check when running setup/start

--- internal/cli/deployments/options/deployment_opts.go
+++ internal/cli/deployments/options/deployment_opts.go
@@ -36,6 +36,7 @@ import (
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/telemetry"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/terminal"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/usage"
+	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/mem"
 )
 
@@ -57,6 +58,7 @@ const (
 	ContainerFilter    = "mongodb-atlas-local=container"
 	bytesInGb          = 1073741824
 	minimumRAM         = 2 * bytesInGb
+	minimumCores       = 2
 )
 
 var (
@@ -159,6 +161,15 @@ func (*DeploymentOpts) ValidateMinimumRequirements() error {
 	if v.Available < minimumRAM {
 		gbOfRAMAvailable := v.Available / (bytesInGb)
 		_, _ = log.Warningf("system does not meet the minimum system requirements: required to have 2GB of ram available. %vGb available.\n", gbOfRAMAvailable)
+	}
+
+	numberOfCores, err := cpu.Counts(true)
+	if err != nil {
+		return err
+	}
+
+	if numberOfCores < minimumCores {
+		_, _ = log.Warningf("system does not meet the minimum system requirements: required to have at least 2 cpu cores. %v cpu cores available.\n", numberOfCores)
 	}
 
 	return nil

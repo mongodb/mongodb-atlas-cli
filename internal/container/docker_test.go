23393567aa3ec3bc0e96c0fe16254e3a6b0964e4
Jeroen Vervaeke <9132134+jeroenvervaeke@users.noreply.github.com>
2024-08-19 09:37:44 +0100

    Update internal/container/docker_test.go
    Co-authored-by: Gustavo Bazan <gustavo.bazan@mongodb.com>

--- internal/container/docker_test.go
+++ internal/container/docker_test.go
@@ -86,6 +86,7 @@ func TestPortMappingFlag(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			result := portMappingFlag(tt.input)
 			if result != tt.expected {
 				t.Errorf("portMappingFlag(%+v) = %s; want %s", tt.input, result, tt.expected)

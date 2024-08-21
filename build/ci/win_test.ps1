f0b7f952d75e9f5779d8c02c4d35aec6304b5fea
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-21 18:53:05 +0100

    add gopath to path

--- build/ci/win_test.ps1
+++ build/ci/win_test.ps1
@@ -1,6 +1,8 @@
 Start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+$goPath = go env GOPATH
+$env:Path += ";$goPath\bin" 
+go install gotest.tools/gotestsum@latest
 cd $env:HOME
 git clone --depth 1 --branch master "http://github.com/mongodb/mongodb-atlas-cli.git"
 cd "mongodb-atlas-cli"
-go install gotest.tools/gotestsum@latest
 gotestsum --junitfile e2e-tests.xml --format standard-verbose -- -timeout 1h -tags e2e -v -run ^TestDeploymentsLocal*$

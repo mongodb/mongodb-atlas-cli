3d59a9eb935aa237b0e1eaf888938f97345aa6d6
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-21 17:06:13 +0100

    add tests

--- build/ci/win_test.ps1
+++ build/ci/win_test.ps1
@@ -1,1 +1,6 @@
-Write-Host "hello from windows"
+Start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+cd $env:HOME
+git clone --depth 1 --branch master "http://github.com/mongodb/mongodb-atlas-cli.git"
+cd "mongodb-atlas-cli"
+go install gotest.tools/gotestsum@latest
+gotestsum --junitfile e2e-tests.xml --format standard-verbose -- -timeout 1h -tags e2e -v -run ^TestDeploymentsLocal*$

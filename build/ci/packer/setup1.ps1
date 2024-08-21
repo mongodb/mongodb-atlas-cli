13e135308f8dd9d6f933d1ec1c93a0c009afbb0b
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-21 17:34:11 +0100

    install git

--- build/ci/packer/setup1.ps1
+++ build/ci/packer/setup1.ps1
@@ -4,5 +4,5 @@ Write-Output 'Configure SSH...'
 (Get-Content 'C:/ProgramData/ssh/sshd_config') -replace '#PubkeyAuthentication yes', 'PubkeyAuthentication yes' -replace '#PasswordAuthentication yes', 'PasswordAuthentication no' | Set-Content 'C:/ProgramData/ssh/sshd_config'
 # choco
 Write-Output 'Install Chocolatey packages...'
-choco install golang -y
+choco install golang git.install -y
 choco install docker-desktop -y --install-arguments="--accept-license --backend=hyper-v --always-run-service"

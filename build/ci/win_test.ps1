6b26f27e76dd30bda4baf76fc6009fb84f4b1efe
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-21 18:20:43 +0100

    fix

--- build/ci/win_test.ps1
+++ build/ci/win_test.ps1
@@ -1,5 +1,4 @@
 Start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-choco install -y git.install
 cd $env:HOME
 git clone --depth 1 --branch master "http://github.com/mongodb/mongodb-atlas-cli.git"
 cd "mongodb-atlas-cli"

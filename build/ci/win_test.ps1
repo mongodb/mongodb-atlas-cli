7a6f2eb95a33897a53170895a5bf89651a184605
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-23 00:41:58 +0100

    run via make

--- build/ci/win_test.ps1
+++ build/ci/win_test.ps1
@@ -2,4 +2,6 @@ Start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 cd $env:HOME
 git clone --depth 1 --branch master "http://github.com/mongodb/mongodb-atlas-cli.git"
 cd "mongodb-atlas-cli"
-go run gotest.tools/gotestsum@latest --junitfile e2e-tests.xml --format standard-verbose -- -v -p 1 -timeout 1h -tags="atlas,deployments,local" ./test/e2e...
+$env:TEST_CMD="go run gotest.tools/gotestsum@latest --junitfile e2e-tests.xml --format standard-verbose --"
+$env:E2E_TAGS="atlas,deployments,local"
+make e2e-test

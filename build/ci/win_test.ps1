f591a026707e4d09d5d440836ae64f9e0a43d738
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-22 20:45:18 +0100

    fix

--- build/ci/win_test.ps1
+++ build/ci/win_test.ps1
@@ -1,6 +1,5 @@
 Start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-go install gotest.tools/gotestsum@latest
 cd $env:HOME
 git clone --depth 1 --branch master "http://github.com/mongodb/mongodb-atlas-cli.git"
 cd "mongodb-atlas-cli"
-gotestsum --junitfile e2e-tests.xml --format standard-verbose -- -v -p 1 -timeout 1h -tags="atlas,deployments,local" ./test/e2e...
+go run gotest.tools/gotestsum@latest --junitfile e2e-tests.xml --format standard-verbose -- -v -p 1 -timeout 1h -tags="atlas,deployments,local" ./test/e2e...

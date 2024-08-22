d0cff6c156984f94c96dd9d241b69ab1e00f3674
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-22 16:48:39 +0100

    fix

--- build/ci/win_test.ps1
+++ build/ci/win_test.ps1
@@ -1,8 +1,6 @@
 Start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-$GOPATH = go env GOPATH
-[Environment]::SetEnvironmentVariable("PATH", "$GOPATH\bin;$env:PATH", "User")
 go install gotest.tools/gotestsum@latest
 cd $env:HOME
 git clone --depth 1 --branch master "http://github.com/mongodb/mongodb-atlas-cli.git"
 cd "mongodb-atlas-cli"
-gotestsum --junitfile e2e-tests.xml --format standard-verbose -- -v -p 1 -timeout 1h -tags="atlas,deployments,local" ./test/e2e...
+"$GOPATH\bin\gotestsum.exe" --junitfile e2e-tests.xml --format standard-verbose -- -v -p 1 -timeout 1h -tags="atlas,deployments,local" ./test/e2e...

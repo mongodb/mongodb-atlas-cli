25cf26169b49d33621c77ab7470555ec31f201f5
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-27 01:51:29 +0100

    disable cgo

--- build/ci/win_test.ps1
+++ build/ci/win_test.ps1
@@ -1,16 +1,19 @@
-Write-Host "Start Docker"
-$env:GOPROXY=$args[0]
+Write-Output "Start Docker"
 Start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-Write-Host "Clone"
+Write-Output "Debug"
+go env
+Write-Output "Clone"
 cd $env:HOME
 git clone "http://github.com/mongodb/mongodb-atlas-cli.git"
 cd "mongodb-atlas-cli"
 git checkout CLOUDP-265394
-Write-Host "Install gotestsum"
+Write-Output "Install gotestsum"
+$env:GOPROXY=$args[0]
+$env:CGO_ENABLED=0
 go install gotest.tools/gotestsum@latest
-Write-Host "Download dependencies"
+Write-Output "Download dependencies"
 go mod download
-Write-Host "Run tests"
+Write-Output "Run tests"
 $env:TEST_CMD="gotestsum --junitfile e2e-tests.xml --format standard-verbose --"
 $env:E2E_TAGS="atlas,deployments,local,auth,noauth,nocli"
 make e2e-test

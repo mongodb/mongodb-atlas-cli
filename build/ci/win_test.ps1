7e55eef52a0585de981bd268476f3875039f1dab
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-08-28 01:32:22 +0100

    update revision and home

--- build/ci/win_test.ps1
+++ build/ci/win_test.ps1
@@ -1,3 +1,18 @@
+<#
+    .SYNOPSIS
+    Runs the e2e test in atlas cli.
+
+    .PARAMETER GOPROXY
+    Specifies the value of GOPROXY environment variable.
+
+    .PARAMETER REVISION
+    Specifies the value of REVISION to be fetched by git.
+#>
+param(
+  [String]$GOPROXY = "https://proxy.golang.org,direct",
+  [String]$REVISION = "HEAD"
+)
+
 Write-Output 'Disable Windows Defender...'
 Set-MpPreference -DisableRealtimeMonitoring $true -Force
 Write-Output "Start Docker"
@@ -6,9 +21,9 @@ Write-Output "Clone"
 cd $env:HOME
 git clone "http://github.com/mongodb/mongodb-atlas-cli.git"
 cd "mongodb-atlas-cli"
-git checkout CLOUDP-265394
+git checkout $REVISION
 Write-Output "Vendor dependencies"
-$env:GOPROXY=$args[0]
+$env:GOPROXY=$GOPROXY
 tar -xzf ../vendor.tar.gz
 Write-Output "Run tests"
 $env:TEST_CMD="gotestsum --junitfile e2e-tests.xml --format standard-verbose --"

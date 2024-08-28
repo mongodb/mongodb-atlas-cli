2716e2f964b510d7884ca2c99a14648a71f239aa
Gustavo Bazan <gustavo.bazan@mongodb.com>
2024-08-28 09:33:30 +0100

    PR comment
    Co-authored-by: lmkerbey-mdb <105309825+lmkerbey-mdb@users.noreply.github.com>

--- internal/cli/accesslogs/list.go
+++ internal/cli/accesslogs/list.go
@@ -35,7 +35,7 @@ const (
 	listTemplate = `HOSTNAME	AUTH RESULT	LOG LINE {{range valueOrEmptySlice .AccessLogs}}
 {{if .Hostname}}{{.Hostname}} {{else}}N/A{{end}}{{.Hostname}}	{{.AuthResult}}	{{.LogLine}}{{end}}
 `
-	invalidValueAuthResultErrorMessage = `--%s must be set to "%s" or "%s"`
+	invalidValueAuthResultErrorMessage = `you must set --%s to "%s" or "%s"`
 )
 
 type ListOpts struct {

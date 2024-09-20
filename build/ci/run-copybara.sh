23698e393d5c47409acd91b03d1bfc310ff8ffac
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-09-20 13:54:48 +0100

    fix env vars

--- build/ci/run-copybara.sh
+++ build/ci/run-copybara.sh
@@ -21,28 +21,28 @@ if [[ "${TAG:?}" == "" ]]; then
     exit 1
 fi
 
-if [[ "${docs_atlas_cli_token:?}" == "" ]]; then
-    echo "missing \$docs_atlas_cli_token"
+if [[ "${DOCS_ATLAS_CLI_TOKEN:?}" == "" ]]; then
+    echo "missing \$DOCS_ATLAS_CLI_TOKEN"
     exit 1
 fi
 
-if [[ "${cloud_docs_token:?}" == "" ]]; then
-    echo "missing \$cloud_docs_token"
+if [[ "${CLOUD_DOCS_TOKEN:?}" == "" ]]; then
+    echo "missing \$CLOUD_DOCS_TOKEN"
     exit 1
 fi
 
-if [[ "${evergreen_user:?}" == "" ]]; then
-    echo "missing \$evergreen_user"
+if [[ "${EVERGREEN_USER:?}" == "" ]]; then
+    echo "missing \$EVERGREEN_USER"
     exit 1
 fi
 
-if [[ "${evergreen_api_key:?}" == "" ]]; then
-    echo "missing \$evergreen_api_key"
+if [[ "${EVERGREEN_API_KEY:?}" == "" ]]; then
+    echo "missing \$EVERGREEN_API_KEY"
     exit 1
 fi
 
-if [[ "${docs_slack_channel:?}" == "" ]]; then
-    echo "missing \$docs_slack_channel"
+if [[ "${DOCS_SLACK_CHANNEL:?}" == "" ]]; then
+    echo "missing \$DOCS_SLACK_CHANNEL"
     exit 1
 fi
 
@@ -56,8 +56,8 @@ EOF
 
 envsubst < copy.bara.sky.template > copy.bara.sky
 
-echo "https://x-access-token:${docs_atlas_cli_token:?}@github.com" > .git-credentials
-echo "https://x-access-token:${docs_atlas_cli_token:?}@api.github.com" >> .git-credentials
+echo "https://x-access-token:${DOCS_ATLAS_CLI_TOKEN:?}@github.com" > .git-credentials
+echo "https://x-access-token:${DOCS_ATLAS_CLI_TOKEN:?}@api.github.com" >> .git-credentials
 
 docker run \
     --name docs-atlas-cli \
@@ -70,8 +70,8 @@ docker run \
 
 DOCS_ATLAS_CLI_PR_URL=$(docker logs docs-atlas-cli 2>&1 | grep "/pull/" | sed -E 's/^.*(https\:.*)$/\1/')
 
-echo "https://x-access-token:${cloud_docs_token:?}@github.com" > .git-credentials
-echo "https://x-access-token:${cloud_docs_token:?}@api.github.com" >> .git-credentials
+echo "https://x-access-token:${CLOUD_DOCS_TOKEN:?}@github.com" > .git-credentials
+echo "https://x-access-token:${CLOUD_DOCS_TOKEN:?}@api.github.com" >> .git-credentials
 
 docker run \
     --name cloud-docs \
@@ -87,15 +87,15 @@ CLOUD_DOCS_PR_URL=$(docker logs cloud-docs 2>&1 | grep "/pull/" | sed -E 's/^.*(
 rm -rf .git-credentials .gitconfig copy.bara.sky
 docker rm -f cloud-docs docs-atlas-cli
 
-TARGET="filipe.menezes" #$docs_slack_channel
+TARGET="filipe.menezes" #$DOCS_SLACK_CHANNEL
 MSG="[TESTING PLEASE IGNORE] Hey team :wave: ${DOCS_ATLAS_CLI_PR_URL} is ready for review :thankyou:"
-curl --header "Api-User:${evergreen_user:?}" \
-    --header "Api-Key:${evergreen_api_key:?}" \
+curl --header "Api-User:${EVERGREEN_USER:?}" \
+    --header "Api-Key:${EVERGREEN_API_KEY:?}" \
     --request POST "https://evergreen.mongodb.com/rest/v2/notifications/slack" \
     --data "{\"target\":\"$TARGET\",\"msg\":\"$MSG\"}"
 
 MSG="[TESTING PLEASE IGNORE] Hey team :wave: ${CLOUD_DOCS_PR_URL} is ready for review :thankyou:"
-curl --header "Api-User:${evergreen_user:?}" \
-    --header "Api-Key:${evergreen_api_key:?}" \
+curl --header "Api-User:${EVERGREEN_USER:?}" \
+    --header "Api-Key:${EVERGREEN_API_KEY:?}" \
     --request POST "https://evergreen.mongodb.com/rest/v2/notifications/slack" \
     --data "{\"target\":\"$TARGET\",\"msg\":\"$MSG\"}"

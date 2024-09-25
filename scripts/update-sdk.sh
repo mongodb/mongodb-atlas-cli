9266e788ee29d245f7a671d9a5dabbb921bdff64
Jeroen Vervaeke <jeroen.vervaeke@mongodb.com>
2024-09-25 17:31:15 +0100

    implemented update-sdk to support pinned versions

--- scripts/update-sdk.sh
+++ scripts/update-sdk.sh
@@ -16,11 +16,21 @@
 
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CURRENT_SDK_RELEASE=$(cat "${SCRIPT_DIR}/../.atlas-sdk-version")
+echo "CURRENT_SDK_RELEASE: $CURRENT_SDK_RELEASE"
+
 LATEST_SDK_TAG=$(curl -sSfL -X GET  https://api.github.com/repos/mongodb/atlas-sdk-go/releases/latest | jq -r '.tag_name')
+echo "LATEST_SDK_TAG: $LATEST_SDK_TAG"
 
 LATEST_SDK_RELEASE=$(echo "${LATEST_SDK_TAG}" | cut -d '.' -f 1)
+echo "LATEST_SDK_RELEASE: $LATEST_SDK_RELEASE"
 echo  "==> Updating SDK to latest major version ${LATEST_SDK_TAG}"
-gomajor get "go.mongodb.org/atlas-sdk/${LATEST_SDK_RELEASE}@${LATEST_SDK_TAG}"
+
+gomajor get --rewrite "go.mongodb.org/atlas-sdk/${CURRENT_SDK_RELEASE}" "go.mongodb.org/atlas-sdk/${LATEST_SDK_RELEASE}@${LATEST_SDK_TAG}"
 go mod tidy
-sed -i -r "s|go.mongodb.org/atlas-sdk/v[0-9]*|go.mongodb.org/atlas-sdk/${LATEST_SDK_RELEASE}|" build/ci/library_owners.json
+
+sed -i -r "s|go.mongodb.org/atlas-sdk/${CURRENT_SDK_RELEASE}|go.mongodb.org/atlas-sdk/${LATEST_SDK_RELEASE}|" build/ci/library_owners.json
+sed -i -r "s|go.mongodb.org/atlas-sdk/${CURRENT_SDK_RELEASE}|go.mongodb.org/atlas-sdk/${LATEST_SDK_RELEASE}|" .golangci.yml
+echo "$LATEST_SDK_RELEASE" > ".atlas-sdk-version"
 echo "Done"

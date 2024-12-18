ca25919d8a3cc0e72a09fd11d409b80e87442737
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-12-18 12:11:33 +0000

    CLOUDP-285952: add support for flex clusters to `atlas backup restores list`

--- internal/cli/backup/restores/list_test.go
+++ internal/cli/backup/restores/list_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/mocks"
 	"github.com/mongodb/mongodb-atlas-cli/atlascli/internal/test"
+	"github.com/stretchr/testify/require"
 	atlasv2 "go.mongodb.org/atlas-sdk/v20241113002/admin"
 )
 
@@ -36,15 +37,42 @@ func TestListOpts_Run(t *testing.T) {
 		clusterName: "Cluster0",
 	}
 
+	expectedError := &atlasv2.GenericOpenAPIError{}
+	expectedError.SetModel(atlasv2.ApiError{ErrorCode: cannotUseNotFlexWithFlexApisErrorCode})
+
+	mockStore.
+		EXPECT().
+		RestoreFlexClusterJobs(listOpts.newListFlexBackupRestoreJobsAPIParams()).
+		Return(nil, expectedError).
+		Times(1)
+
 	mockStore.
 		EXPECT().
 		RestoreJobs(listOpts.ProjectID, "Cluster0", listOpts.NewListOptions()).
 		Return(expected, nil).
 		Times(1)
 
-	if err := listOpts.Run(); err != nil {
-		t.Fatalf("Run() unexpected error: %v", err)
+	require.NoError(t, listOpts.Run())
+	test.VerifyOutputTemplate(t, restoreListTemplate, expected)
+}
+
+func TestListOpts_Run_FlexCluster(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockStore := mocks.NewMockRestoreJobsLister(ctrl)
+
+	expected := &atlasv2.PaginatedApiAtlasFlexBackupRestoreJob20241113{}
+
+	listOpts := &ListOpts{
+		store:       mockStore,
+		clusterName: "Cluster0",
 	}
 
-	test.VerifyOutputTemplate(t, restoreListTemplate, expected)
+	mockStore.
+		EXPECT().
+		RestoreFlexClusterJobs(listOpts.newListFlexBackupRestoreJobsAPIParams()).
+		Return(nil, nil).
+		Times(1)
+
+	require.NoError(t, listOpts.Run())
+	test.VerifyOutputTemplate(t, restoreListFlexClusterTemplate, expected)
 }

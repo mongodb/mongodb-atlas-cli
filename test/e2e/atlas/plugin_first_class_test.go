585205e0c2c30e6b48ae91e448d8942b5522919c
Melanija Cvetic <melanija.cvetic@mongodb.com>
2025-01-16 16:23:45 +0000

    Initial test

---
+++ test/e2e/atlas/plugin_first_class_test.go
@@ -0,0 +1,54 @@
+// Copyright 2025 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build e2e || kubernetes || (atlas && plugin)
+
+package atlas_test
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/mongodb/mongodb-atlas-cli/atlascli/test/e2e"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPluginKubernetes(t *testing.T) {
+	t.Helper()
+	cliPath, err := e2e.AtlasCLIBin()
+	require.NoError(t, err)
+
+	t.Run("should install K8s plugin", func(t *testing.T) {
+		removeFirstClassPlugin(t, "atlas-cli-plugin-kubernetes", cliPath)
+
+		cmd := exec.Command(cliPath,
+			"kubernetes")
+		cmd.Env = os.Environ()
+		resp, err := cmd.CombinedOutput()
+		require.NoError(t, err, string(resp))
+		assert.Contains(t, string(resp), "Plugin mongodb/atlas-cli-plugin-kubernetes successfully installed\n")
+	})
+}
+
+func removeFirstClassPlugin(t *testing.T, name, cliPath string) {
+	t.Helper()
+	cmd := exec.Command(cliPath,
+		"plugin",
+		"uninstall",
+		name)
+	resp, err := cmd.CombinedOutput()
+	require.NoError(t, err, string(resp))
+}

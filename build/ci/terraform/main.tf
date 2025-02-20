017310614c54a755a95adacb467a900ac92786f5
Filipe C Menezes <filipe.menezes@mongodb.com>
2025-02-20 16:15:35 +0000

    fix

--- build/ci/terraform/main.tf
+++ build/ci/terraform/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_network_interface" "atlascli_vm_nic" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.atlascli_vm_subnet.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
     public_ip_address_id          = azurerm_public_ip.atlascli_vm_pip.id
   }
 }

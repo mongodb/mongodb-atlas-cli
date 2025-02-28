76dfef759de87ac616498f93b715ee102f8ed53b
Filipe C Menezes <filipe.menezes@mongodb.com>
2025-02-28 12:06:56 +0000

    fix

--- internal/cli/api/strings.go
+++ internal/cli/api/strings.go
@@ -22,7 +22,8 @@ import (
 func splitShortAndLongDescription(description string) (string, string) {
 	// Split on periods that are followed by a space or end of string
 	// This approach allows us to not accidentally split verion numbers like 8.0
-	split := regexp.MustCompile(`\.(?:\s+|$)`).Split(description, -1)
+	const maxSegments = 2
+	split := regexp.MustCompile(`\.(?:\s+|$)`).Split(description, maxSegments)
 
 	// Short description is the first sentence
 	shortDescription := split[0]
@@ -36,18 +37,8 @@ func splitShortAndLongDescription(description string) (string, string) {
 	longDescription := ""
 
 	if len(split) > 1 {
-		// Remove all empty whitespace around sentences
-		// This turns multi-line descriptions into single line
-		for i, s := range split[1:] {
-			split[i+1] = strings.TrimSpace(s)
-		}
-
-		// Add the ". " back
-		longDescription = strings.Join(split[1:], ". ")
+		longDescription = strings.TrimSpace(split[1])
 	}
-
-	// Get rid of the last space after ". "
-	longDescription = strings.TrimSpace(longDescription)
 
 	return shortDescription, longDescription
 }

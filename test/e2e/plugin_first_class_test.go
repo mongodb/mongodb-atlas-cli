5398a45c69502854fd963ceee23fca47812a1a4a
andreaangiolillo <andrea.angiolillo@mongodb.com>
2025-03-28 11:31:08 +0000

    Fix kub planing e21e tests

--- test/e2e/plugin_first_class_test.go
+++ test/e2e/plugin_first_class_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build e2e || kubernetes
+//go:build e2e || kubernetes || (atlas && plugin)
 
 package e2e_test
 

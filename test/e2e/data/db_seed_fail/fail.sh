25de1275f123b51ca320d06aebce468d423e78ba
Jeroen Vervaeke <9132134+jeroenvervaeke@users.noreply.github.com>
2025-04-07 12:11:28 +0100

    CLOUDP-306589: Extend `--debug` to include Docker logs on `atlas deployments setup --type local` (#3807)

---
+++ test/e2e/data/db_seed_fail/fail.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright 2025 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Sleep for 10 seconds
+sleep 10
+
+# Echo some tokens we can find in the e2e test
+echo "CATASTROPHIC_SEED_FAILURE_PANIC_DONT_PANIC"
+
+# Exit with code 1
+exit 1

bc4398f8bb544b9a80d8055483d36fcdd951177e
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2025-05-20 18:43:43 +0100

    update

--- build/ci/gen-ssdlc-report.sh
+++ build/ci/gen-ssdlc-report.sh
@@ -20,7 +20,7 @@ release_date=${DATE:-$(date -u '+%Y-%m-%d')}
 
 export DATE="${release_date}"
 VERSION=""
-VERSION=$(git tag --list 'atlascli/v*' --sort=-taggerdate | head -1 )
+VERSION=$(git tag --list 'atlascli/v*' --sort=-taggerdate | head -1 | cut -d 'v' -f 2)
 export VERSION
 export AUTHOR="${AUTHOR:-$(git config user.name)}"
 

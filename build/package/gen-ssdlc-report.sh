15865d951ec6ef5df50269c7c55ac361fa660350
Bianca Lisle <40155621+blva@users.noreply.github.com>
2025-05-22 12:23:05 +0100

    CLOUDP-319858: Auto-update SBOM report in codebase (#3902)

--- build/package/gen-ssdlc-report.sh
+++ build/package/gen-ssdlc-report.sh
@@ -19,22 +19,28 @@ set -eu
 release_date=${DATE:-$(date -u '+%Y-%m-%d')}
 
 export DATE="${release_date}"
-VERSION=""
-VERSION=$(git tag --list 'atlascli/v*' --sort=-taggerdate | head -1 | cut -d 'v' -f 2)
+
+if [ -z "${AUTHOR:-}" ]; then
+  AUTHOR=$(git config user.name)
+fi
+
+if [ -z "${VERSION:-}" ]; then
+  VERSION=$(git tag --list 'atlascli/v*' --sort=-taggerdate | head -1 | cut -d 'v' -f 2)
+fi
+
+export AUTHOR
 export VERSION
-export AUTHOR="${AUTHOR:-$(git config user.name)}"
 
 echo "Generating SSDLC checklist for AtlasCLI version ${VERSION}, author ${AUTHOR} and release date ${DATE}..."
 
-# Ensure compliance directory exists
-mkdir -p "compliance/"
+# Ensure AtlasCLI version directory exists
+mkdir -p "compliance/v${VERSION}"
 
-# Generate the report in compliance/ with a versioned filename
 envsubst < docs/releases/ssdlc-compliance.template.md \
-  > "compliance/ssdlc-compliance-${VERSION}.md"
+  > "compliance/v${VERSION}/ssdlc-compliance-${VERSION}.md"
 
-echo "SDLC checklist ready. Files in compliance/:"
-ls -l "compliance/"
+echo "SDLC checklist ready. Files in compliance/v${VERSION}/:"
+ls -l "compliance/v${VERSION}/"
 
 echo "Printing the generated report:"
-cat "compliance/ssdlc-compliance-${VERSION}.md"
+cat "compliance/v${VERSION}/ssdlc-compliance-${VERSION}.md"

6b7cf3afdf16e6f6e9e70582a753f970b34748ec
Filipe C Menezes <filipe.menezes@mongodb.com>
2025-05-30 14:32:00 +0100

    add remote host

---
+++ build/package/windows_build_msi.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Copyright 2020 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -Eeou pipefail
+
+HOSTNAME=$(jq -r '.hosts[0].dns_name' < hosts.json)
+USERNAME=Administrator
+
+identity_file=~/.ssh/mcipacker.pem
+attempts=0
+connection_attempts=${connection_attempts|25}
+
+while ! ssh \
+    -i "$identity_file" \
+    -o ConnectTimeout=10 \
+    -o ForwardAgent=yes \
+    -o IdentitiesOnly=yes \
+    -o StrictHostKeyChecking=no \
+    "$(printf "%s@%s" "$USERNAME" "$HOSTNAME")" \
+    exit
+do
+    [ "$attempts" -ge "$connection_attempts" ] && printf "SSH connection attempt %d/%d failed." "$attempts" "$connection_attempts" && exit 1
+    ((attempts++))
+    printf "SSH connection attempt %d/%d failed. Retrying...\n" "$attempts" "$connection_attempts"
+    sleep 10
+done
+
+ssh \
+    -i "$identity_file" \
+    -o ConnectTimeout=10 \
+    -o ForwardAgent=yes \
+    -o IdentitiesOnly=yes \
+    -o StrictHostKeyChecking=no \
+    "$(printf "%s@%s" "$USERNAME" "$HOSTNAME")" \
+    bash -c 'echo "echo from remote host"'

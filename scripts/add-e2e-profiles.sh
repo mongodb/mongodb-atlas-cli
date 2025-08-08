bd07af1f7f8523ef800a489c92a7089b488dcd01
Filipe C Menezes <filipe.menezes@mongodb.com>
2025-08-08 01:04:22 +0100

    fix

--- scripts/add-e2e-profiles.sh
+++ scripts/add-e2e-profiles.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Copyright 2025 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -euo pipefail
 
@@ -9,7 +22,8 @@ set -euo pipefail
 
 ./bin/atlas config delete __e2e_snapshot --force >/dev/null 2>&1 || true
 
-CONFIG_PATH=$(EDITOR=echo atlas config edit 2>/dev/null)
+EDITOR=echo
+CONFIG_PATH=$(atlas config edit 2>/dev/null)
 
 cat <<EOF >> "$CONFIG_PATH"
 

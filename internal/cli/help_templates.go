53b81e63210985d17080fb3bc672c91bfd7faba9
Luke Sanderson <luke.sanderson@mongodb.com>
2025-09-26 11:27:01 +0100

    Variable Rename and re-add long-text api autogeneration disclaimer

--- internal/cli/help_templates.go
+++ internal/cli/help_templates.go
@@ -21,10 +21,10 @@ const (
 
 {{if or .Runnable .HasSubCommands}}{{.UsageString}}{{end}}`
 
-	ExperimentalText = "The atlas api sub-command, " +
+	APICommandText = "The atlas api sub-command, " +
 		"automatically generated from the MongoDB Atlas Admin API, offers full coverage of the Admin API.\n" +
 		"Admin API capabilities have their own release lifecycle, which you can check via the provided API endpoint " +
 		"documentation link.\n\n"
 
-	APICommandHelpTemplate = ExperimentalText + HelpTemplate
+	APICommandHelpTemplate = APICommandText + HelpTemplate
 )

ec1bbcb59c62a46eb77f2a38a1d5cc5bfd0539b0
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2026-01-02 17:41:02 +0000

    CLOUDP-362051: update azure peering docs URL

--- internal/cli/networking/peering/create/azure.go
+++ internal/cli/networking/peering/create/azure.go
@@ -142,7 +142,7 @@ func AzureBuilder() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "azure",
 		Short: "Create a network peering connection between the Atlas VPC and your Azure VNet.",
-		Long: `Before you create an Azure network peering connection, complete the prerequisites listed here: https://www.mongodb.com/docs/atlas/reference/api/vpc-create-peering-connection/#prerequisites.
+		Long: `Before you create an Azure network peering connection, complete the prerequisites listed here: https://www.mongodb.com/docs/atlas/security-vpc-peering/#aws-vpc-peering-prerequisites.
 		
 The network peering create command checks if a VNet exists in the region you specify for your Atlas project. If one exists, this command creates the peering connection between that VNet and your VPC. If an Atlas VPC does not exist, this command creates one and creates a connection between it and your VNet.
 		

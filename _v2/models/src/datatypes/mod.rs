6df351571aa2b5ef5163d9f5bce04f6af7f461c2
Jeroen Vervaeke <jeroen.vervaeke@mongodb.com>
2026-09-15 15:28:16 +0100

    add DataType enum

--- _v2/models/src/datatypes/mod.rs
+++ _v2/models/src/datatypes/mod.rs
@@ -1,2 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+use crate::datatypes::{reference_types::ReferenceType, value_type::ValueType};
+
 pub mod reference_types;
 pub mod value_type;
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum DataType {
+    ReferenceType(ReferenceType),
+    ValueType(ValueType),
+}
